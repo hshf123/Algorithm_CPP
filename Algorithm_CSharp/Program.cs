7d1c7d8e803c5165ed3dca1341413b1708afb79f
HanSanghyun <hshf123@naver.com>
2023-01-01 21:03:53 +0900

    Hello World

--- Algorithm_CSharp/Program.cs
+++ Algorithm_CSharp/Program.cs
@@ -21,11 +21,11 @@ namespace Algorithm_CS
 
             string name = type.Name; // 클래스 이름 추출
             FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.Instance); // 필드 추출
-            foreach(FieldInfo fieldInfo in fieldInfos)
+            foreach (FieldInfo fieldInfo in fieldInfos)
             {
-                if(fieldInfo.IsPublic)
+                if (fieldInfo.IsPublic)
                     Console.WriteLine($"public {fieldInfo.FieldType.Name} {fieldInfo.Name}");
-                else if(fieldInfo.IsPrivate)
+                else if (fieldInfo.IsPrivate)
                     Console.WriteLine($"private {fieldInfo.FieldType.Name} {fieldInfo.Name}");
                 else
                     Console.WriteLine($"protected {fieldInfo.FieldType.Name} {fieldInfo.Name}");
