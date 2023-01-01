b1113d5b604d7589ef48c2e4325c2ae3ab1b72d2
HanSanghyun <hshf123@naver.com>
2023-01-01 21:30:44 +0900

    7287

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -1,19 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <time.h>
 
 int main()
 {
-	time_t timer;
-	timer = time(NULL);
-
-	struct tm* t = localtime(&timer);
-
-	std::cout << t->tm_year + 1900 << "-";
-	std::cout.width(2);
-	std::cout.fill('0');
-	std::cout << t->tm_mon + 1 << "-";
-	std::cout.width(2);
-	std::cout.fill('0');
-	std::cout << t->tm_mday;
+	std::cout << 40 << std::endl << "hshf123";
 }
