09561f221d8ae54eec0df1a18f192c855a057478
HanSanghyun <hshf123@naver.com>
2023-01-01 23:56:36 +0900

    2754

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -9,16 +9,31 @@ int main()
 
 	string str;
 	cin >> str;
+	float i;
 
-	for (int i = 0; i < str.length(); i++)
+	if (str[0] == 'A')
+		i = 4;
+	else if (str[0] == 'B')
+		i = 3;
+	else if (str[0] == 'C')
+		i = 2;
+	else if (str[0] == 'D')
+		i = 1;
+	else
 	{
-		if (str[i] == tolower(str[i]))
-			str[i] = toupper(str[i]);
-		else
-			str[i] = tolower(str[i]);
+		cout << fixed;
+		cout.precision(1); 
+		cout << 0.0;
+		return 0;
 	}
 
-	cout << str;
+	if (str[1] == '+')
+		i += 0.3f;
+	else if(str[1] == '-')
+		i -= 0.3f;
+	cout << fixed;
+	cout.precision(1);
+	cout << i;
 
 	return 0;
 }
