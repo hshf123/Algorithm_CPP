ec915844dbe716307dc575119709065b8d7bc1e3
HanSanghyun <hshf123@naver.com>
2023-01-01 21:49:45 +0900

    1000

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main()
 {
-	cout << "         ,r\'\"7" << endl;
-	cout << "r`-_   ,'  ,/" << endl;
-	cout << " \\. \". L_r\'" << endl;
-	cout << "   `~\\/" << endl;
-	cout << "      |" << endl;
-	cout << "      |" << endl;
+	int a;
+	int b;
+	cin >> a >> b;
+
+	cout << a + b;
 }
