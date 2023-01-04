362b0ba67d8350db1b2e6b363d49f9c76926ea9c
HanSanghyun <hshf123@naver.com>
2023-01-04 23:47:23 +0900

    Graph Cycle profiler

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -2,25 +2,78 @@
 using namespace std;
 #define endl "\n"
 
-#include <thread>
-#include <atomic>
-#include <mutex>
+#include <map>
+#include <unordered_map>
+#include <set>
 #include <vector>
 
-mutex m;
+vector<vector<int>>			_vec;
+map<int, set<int>>			_history;
+vector<int>					_visitedOrder;
+int							_visitedCount;
+vector<bool>				_finished;
+vector<int>					_parent;
 
-bool IsPrime(int number)
+void DFS(int here)
 {
-	if (number <= 1)
-		return false;
-	if (number == 2 || number == 3)
-		return true;
+	// 이미 방문한 곳이면 pass
+	if (_visitedOrder[here] != -1)
+		return;
 
-	for (int i = 2; i < number; i++)
-		if (number % i == 0)
-			return false;
+	// 몇 번째로 방문한 곳인지를 기입
+	cout << here << "는(은) " << _visitedCount + 1 << "번째" << endl;
+	_visitedOrder[here] = _visitedCount++;
 
-	return true;
+	auto findIt = _history.find(here);
+	if (findIt == _history.end())
+	{
+		// 연결된 노드들이 없다.
+		_finished[here] = true;
+		return;
+	}
+
+	// 연결된 노드가 있다.
+	set<int>& next = findIt->second;
+	for (int there : next)
+	{
+		if (_visitedOrder[there] == -1)
+		{
+			// 연결된 노드를 아직 방문한 적이 없다
+			// 연결된 노드의 부모를 현재 위치로 설정 (추적용)
+			_parent[there] = here;
+			DFS(there);
+			continue;
+		}
+
+		// 순방향 간선인 상황 (방문 순번으로 판단)
+		if (_visitedOrder[here] < _visitedOrder[there])
+			continue;
+
+		// 역방향 간선
+		if (_finished[there] == false) 
+		{
+			// DFS가 아직 끝나지 않은 상황 
+			// 만약 _finished[there]이 true라면 교차간선임
+			cout << "Cycle Detected!!" << endl;
+			cout << here << " -> " << there << endl;
+
+			int now = here;
+			while (now != there)
+			{
+				// 부모를 추적해서 어떻게 사이클이 형성되었는지 출력
+				cout << _parent[now] << " -> " << now << endl;
+				now = _parent[now];
+			}
+		}
+	}
+	_finished[here] = true;
+}
+
+void Push(int node, int edge)
+{
+	_vec[node].push_back(edge);
+	set<int>& history = _history[node];
+	history.insert(edge);
 }
 
 int main()
@@ -28,22 +81,17 @@ int main()
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	const int MAX_NUMBER = 100'0000;
-	atomic<int> num = 0;
-	atomic<int> primeCount = 0;
+	_visitedOrder = vector<int>(5, -1);
+	_visitedCount = 0;
+	_finished = vector<bool>(5, false);
+	_parent = vector<int>(5, -1);
 
-	vector<thread> threads;
-	int coreCount = thread::hardware_concurrency();
-	for (int i = 0; i < coreCount; i++)
-		threads.push_back(thread([&num, &primeCount]()
-			{
-				while (num <= MAX_NUMBER)
-					if (IsPrime(num++))
-						primeCount++;
-			}));
+	_vec.resize(5);
+	Push(0, 1);
+	Push(0, 2);
+	Push(1, 3);
+	Push(3, 0);
+	Push(4, 2);
 
-	for (int i = 0; i < coreCount; i++)
-		threads[i].join();
-
-	cout << primeCount << endl;
+	DFS(0);
 }
