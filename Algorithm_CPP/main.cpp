dc258ccfdc4967e10a0a66a0083ef4733055af03
HanSanghyun <hshf123@naver.com>
2023-01-05 02:03:37 +0900

    2108

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -1,97 +1,75 @@
 #include <iostream>
 using namespace std;
 #define endl "\n"
+#define int64 long long
 
+#include <cmath>
 #include <map>
-#include <unordered_map>
-#include <set>
 #include <vector>
-
-vector<vector<int>>			_vec;
-map<int, set<int>>			_history;
-vector<int>					_visitedOrder;
-int							_visitedCount;
-vector<bool>				_finished;
-vector<int>					_parent;
-
-void DFS(int here)
-{
-	// 이미 방문한 곳이면 pass
-	if (_visitedOrder[here] != -1)
-		return;
-
-	// 몇 번째로 방문한 곳인지를 기입
-	cout << here << "는(은) " << _visitedCount + 1 << "번째" << endl;
-	_visitedOrder[here] = _visitedCount++;
-
-	auto findIt = _history.find(here);
-	if (findIt == _history.end())
-	{
-		// 연결된 노드들이 없다.
-		_finished[here] = true;
-		return;
-	}
-
-	// 연결된 노드가 있다.
-	set<int>& next = findIt->second;
-	for (int there : next)
-	{
-		if (_visitedOrder[there] == -1)
-		{
-			// 연결된 노드를 아직 방문한 적이 없다
-			// 연결된 노드의 부모를 현재 위치로 설정 (추적용)
-			_parent[there] = here;
-			DFS(there);
-			continue;
-		}
-
-		// 순방향 간선인 상황 (방문 순번으로 판단)
-		if (_visitedOrder[here] < _visitedOrder[there])
-			continue;
-
-		// 역방향 간선
-		if (_finished[there] == false) 
-		{
-			// DFS가 아직 끝나지 않은 상황 
-			// 만약 _finished[there]이 true라면 교차간선임
-			cout << "Cycle Detected!!" << endl;
-			cout << here << " -> " << there << endl;
-
-			int now = here;
-			while (now != there)
-			{
-				// 부모를 추적해서 어떻게 사이클이 형성되었는지 출력
-				cout << _parent[now] << " -> " << now << endl;
-				now = _parent[now];
-			}
-		}
-	}
-	_finished[here] = true;
-}
-
-void Push(int node, int edge)
-{
-	_vec[node].push_back(edge);
-	set<int>& history = _history[node];
-	history.insert(edge);
-}
+#include <algorithm>
 
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	_visitedOrder = vector<int>(5, -1);
-	_visitedCount = 0;
-	_finished = vector<bool>(5, false);
-	_parent = vector<int>(5, -1);
+	int N;
+	cin >> N;
+	map<int, int> _numToCount;
+	vector<vector<int>> _mode(N);
+	vector<int> _mid(N, 0);
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		auto findIt = _numToCount.find(num);
+		if (findIt == _numToCount.end())
+		{
+			_numToCount.insert(make_pair(num, 1));
+			_mode[0].push_back(num);
+		}
+		else
+		{
+			// 최소 1번 이상 등장한 숫자
+			findIt->second++;
+			_mode[findIt->second - 1].push_back(findIt->first);
+		}
 
-	_vec.resize(5);
-	Push(0, 1);
-	Push(0, 2);
-	Push(1, 3);
-	Push(3, 0);
-	Push(4, 2);
+		_mid[i] = num;
+	}
 
-	DFS(0);
+
+	double sum = 0;
+	int max = 0;
+	for (auto it = _numToCount.begin(); it != _numToCount.end(); ++it)
+	{
+		sum += it->first * it->second;
+
+		if (it->second > max)
+			max = it->second;
+	}
+
+	double avg = 0;
+	avg = sum / N;
+	avg = floor(avg + 0.5);
+
+	sort(_mid.begin(), _mid.end());
+	int mid = _mid[N / 2];
+
+	int mode = 0;
+	if (_mode[max - 1].size() > 1)
+	{
+		sort(_mode[max - 1].begin(), _mode[max - 1].end());
+		mode = _mode[max - 1][1];
+	}
+	else
+	{
+		mode = _mode[max - 1][0];
+	}
+
+	int range = (--_numToCount.end())->first - _numToCount.begin()->first;
+	cout << avg << endl;
+	cout << mid << endl;
+	cout << mode << endl;
+	cout << range << endl;
 }
