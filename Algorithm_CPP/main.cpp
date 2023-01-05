9e0db0f68c85a66070c81a7e03c9fd78b15680b3
HanSanghyun <hshf123@naver.com>
2023-01-05 22:56:17 +0900

    11050

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -11,87 +11,30 @@ using namespace std;
 #include <set>
 #include <stack>
 
+
+int Factorial(int n)
+{
+	int res = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		res *= i;
+	}
+	return res;
+}
+
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int N;
-	cin >> N;
+	int N, K;
+	cin >> N >> K;
 	
-	queue<string> _commands;
-	for (int i = 0; i <= N; i++)
+	if (K < 0 || K > N)
 	{
-		string command;
-		getline(cin, command);
-		_commands.push(command);
+		cout << "0";
+		return 0;
 	}
 
-	deque<int> _deque;
-	_commands.pop();
-	while (_commands.empty() == false)
-	{
-		string& str = _commands.front();
-
-		if (str == "pop_front")
-		{
-			if (_deque.empty())
-				cout << "-1" << endl;
-			else
-			{
-				cout << _deque.front() << endl;
-				_deque.pop_front();
-			}
-		}
-		else if (str == "pop_back")
-		{
-			if (_deque.empty())
-				cout << "-1" << endl;
-			else
-			{
-				cout << _deque.back() << endl;
-				_deque.pop_back();
-			}
-		}
-		else if (str == "size")
-		{
-			cout << _deque.size() << endl;
-		}
-		else if (str == "empty")
-		{
-			if (_deque.empty())
-				cout << "1" << endl;
-			else
-				cout << "0" << endl;
-		}
-		else if (str == "front")
-		{
-			if (_deque.empty())
-				cout << "-1" << endl;
-			else
-				cout << _deque.front() << endl;
-		}
-		else if (str == "back")
-		{
-			if (_deque.empty())
-				cout << "-1" << endl;
-			else
-				cout << _deque.back() << endl;
-		}
-		else
-		{
-			// push
-			int index = str.find(" ");
-			string forb = str.substr(0, index);
-			string strNum = str.substr(index, str.size() - 1);
-			int n = stoi(strNum);
-
-			if (forb == "push_back")
-				_deque.push_back(n);
-			else
-				_deque.push_front(n);
-		}
-
-		_commands.pop();
-	}
+	cout << Factorial(N) / (Factorial(K) * Factorial(N - K));
 }
