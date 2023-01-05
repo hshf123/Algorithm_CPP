a1c642c5bd953b65efdfd9b51d48f657508f22ad
HanSanghyun <hshf123@naver.com>
2023-01-05 22:49:25 +0900

    10866

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -27,54 +27,69 @@ int main()
 		_commands.push(command);
 	}
 
-	queue<int> _queue;
+	deque<int> _deque;
 	_commands.pop();
 	while (_commands.empty() == false)
 	{
 		string& str = _commands.front();
 
-		if (str == "pop")
+		if (str == "pop_front")
 		{
-			if (_queue.empty())
+			if (_deque.empty())
 				cout << "-1" << endl;
 			else
 			{
-				cout << _queue.front() << endl;
-				_queue.pop();
+				cout << _deque.front() << endl;
+				_deque.pop_front();
+			}
+		}
+		else if (str == "pop_back")
+		{
+			if (_deque.empty())
+				cout << "-1" << endl;
+			else
+			{
+				cout << _deque.back() << endl;
+				_deque.pop_back();
 			}
 		}
 		else if (str == "size")
 		{
-			cout << _queue.size() << endl;
+			cout << _deque.size() << endl;
 		}
 		else if (str == "empty")
 		{
-			if (_queue.empty())
+			if (_deque.empty())
 				cout << "1" << endl;
 			else
 				cout << "0" << endl;
 		}
 		else if (str == "front")
 		{
-			if (_queue.empty())
+			if (_deque.empty())
 				cout << "-1" << endl;
 			else
-				cout << _queue.front() << endl;
+				cout << _deque.front() << endl;
 		}
 		else if (str == "back")
 		{
-			if (_queue.empty())
+			if (_deque.empty())
 				cout << "-1" << endl;
 			else
-				cout << _queue.back() << endl;
+				cout << _deque.back() << endl;
 		}
 		else
 		{
 			// push
 			int index = str.find(" ");
-			string s = str.substr(index, str.size() - 1);
-			int n = stoi(s);
-			_queue.push(n);
+			string forb = str.substr(0, index);
+			string strNum = str.substr(index, str.size() - 1);
+			int n = stoi(strNum);
+
+			if (forb == "push_back")
+				_deque.push_back(n);
+			else
+				_deque.push_front(n);
 		}
 
 		_commands.pop();
