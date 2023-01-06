8273318f4d7027dbbc023eb405ceb26c4cb724d2
HanSanghyun <hshf123@naver.com>
2023-01-06 20:02:10 +0900

    Memory Stomp

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+//using namespace std;
 #define endl "\n"
 using int64 = long long;
 
@@ -12,46 +12,51 @@ using int64 = long long;
 #include <stack>
 #include <list>
 
+//int main()
+//{
+//	cin.tie(NULL);
+//	ios::sync_with_stdio(false);
+//
+//	LeafC* leafC = new LeafC();
+//	leafC->number = 100;
+//	leafC->number2 = 100;
+//	delete leafC;
+//	leafC->number = 200;
+//}
+
+#include <windows.h>
+using int64 = long long;
+const __int16 PAGE_SIZE = 0x1000; // 4096
+
+class LeafCParent
+{
+public:
+	int parentNumber = 20;
+};
+
+class LeafC : public LeafCParent
+{
+public:
+	int number = 10;
+};
+
+void* Alloc(int size)
+{
+	const int64 pageCount = (size + PAGE_SIZE - 1) / PAGE_SIZE;
+	void* ptr = ::VirtualAlloc(NULL, pageCount * PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	ptr = static_cast<__int8*>(ptr) + (pageCount * PAGE_SIZE - size);
+	return static_cast<void*>(ptr);
+}
+
+template<typename Type, typename... Args>
+Type* x_new(Args&&... args)
+{
+	Type* memory = static_cast<Type*>(Alloc(sizeof(Type)));
+	new(memory)Type(std::forward<Args>(args)...);
+	return memory;
+}
+
 int main()
 {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	int N, M;
-	cin >> N >> M;
-	
-	vector<int> vec(N);
-	for (int i = 0; i < N; i++)
-	{
-		int H;
-		cin >> H;
-		vec[i] = H;
-	}
-	sort(vec.begin(), vec.end());
-
-	int height = 0;
-	int left = 0;
-	int right = vec.back();
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		int64 sum = 0;
-		for (int i = 0; i < N; i++)
-		{
-			int tree = vec[i];
-			sum += (tree - mid > 0 ? tree - mid : 0);
-		}
-		if (sum >= M)
-		{
-			if (height < mid)
-				height = mid;
-			left = mid + 1;
-		}
-		else
-		{
-			right = mid - 1;
-		}
-	}
-
-	cout << height;
+	LeafC* leafC = x_new<LeafC>();
 }
