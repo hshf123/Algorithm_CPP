4d0421837141894fc3a428b3a8481df7a067533c
HanSanghyun <hshf123@naver.com>
2023-01-08 22:29:33 +0900

    15829

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -13,6 +13,7 @@ using int64 = long long;
 #include <unordered_set>
 #include <string>
 #include <algorithm>
+#include <cmath>
 
 int main()
 {
@@ -21,25 +22,20 @@ int main()
 
 	int N;
 	cin >> N;
-	// y, x
-	map<int, priority_queue<int, vector<int>, greater<int>>> _map;
+	string str;
+	cin >> str;
+	const char* p = str.c_str();
+	int64 sum = 0;
+	int64 r = 1;
 	for (int i = 0; i < N; i++)
 	{
-		int x, y;
-		cin >> x >> y;
-		_map[y].push(x);
+		char c = *(p + i);
+		short number = c - 'a' + 1;
+		sum = (sum + number * r) % 1234567891;
+		r = (r * 31) % 1234567891;
 	}
 
-	for (auto it = _map.begin(); it != _map.end(); ++it)
-	{
-		int y = it->first;
-		while (it->second.empty() == false)
-		{
-			int x = it->second.top();
-			it->second.pop();
-			cout << x << " " << y << endl;
-		}
-	}
+	cout << sum;
 
 	return 0;
 }
