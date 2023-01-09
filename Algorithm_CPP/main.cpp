4dc908dc34c706564a400ca4df241f03b7e9af60
HanSanghyun <hshf123@naver.com>
2023-01-09 12:35:47 +0900

    1074

--- Algorithm_CPP/main.cpp
+++ Algorithm_CPP/main.cpp
@@ -15,35 +15,42 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
-vector<vector<bool>> vec;
-vector<vector<bool>> visited;
-vector<vector<pair<int, int>>> parent;
-int xdir[] = { 0, 1, 0, -1 };
-int ydir[] = { -1, 0, 1, 0 };
-int earthworm = 0;
-int T;
-int M, N, K;
+int number = 0;
+int N, r, c;
 
-void DFS(int x, int y)
+void DIV(int startx, int starty, int endx, int endy)
 {
-	visited[y][x] = true;
+	int n = endx - startx;
+	int divNumber = n / 2; // 2^n-1 * 2^n-1
 
-	if (vec[y][x] == true)
+	if (n == 2)
 	{
-		for (int i = 0; i < 4; i++)
-		{
-			int dx = xdir[i] + x;
-			int dy = ydir[i] + y;
+		int y = r - starty;
+		y *= 2;
+		int x = c - startx;
+		number += (x + y);
+		cout << number;
+		return;
+	}
 
-			if (dx >= 0 && dx < M && dy >= 0 && dy < N)
-			{
-				if (vec[dy][dx] == true && visited[dy][dx] == false)
-				{
-					parent[dy][dx] = make_pair(y, x);
-					DFS(dx, dy);
-				}
-			}
-		}
+	if (starty <= r && r < endy - divNumber && startx <= c && c < endx - divNumber)
+	{
+		DIV(startx, starty, endx - divNumber, endy - divNumber);
+	}
+	else if (starty <= r && r < endy - divNumber && startx + divNumber <= c && c < endx)
+	{
+		number += divNumber * divNumber;
+		DIV(startx + divNumber, starty, endx, endy - divNumber);
+	}
+	else if (starty + divNumber <= r && r < endy && startx <= c && c < endx - divNumber)
+	{
+		number += divNumber * divNumber * 2;
+		DIV(startx, starty + divNumber, endx - divNumber, endy);
+	}
+	else
+	{
+		number += divNumber * divNumber * 3;
+		DIV(startx + divNumber, starty + divNumber, endx, endy);
 	}
 }
 
@@ -52,55 +59,11 @@ int main()
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> T;
-	
-	for (int testcase = 0; testcase < T; testcase++)
-	{
-		vec.clear();
-		visited.clear();
-		earthworm = 0;
+	cin >> N >> r >> c;
 
-		cin >> M >> N >> K;
+	int n = pow(2, N);
 
-		vec = vector<vector<bool>>(N, vector<bool>(M, false));
-		visited = vector<vector<bool>>(N, vector<bool>(M, false));
-		parent = vector<vector<pair<int, int>>>(N, vector<pair<int, int>>(M, make_pair(-1, -1)));
-
-		for (int pos = 0; pos < K; pos++)
-		{
-			int x, y;
-			cin >> x >> y;
-			vec[y][x] = true;
-		}
-
-		for (int y = 0; y < N; y++)
-		{
-			for (int x = 0; x < M; x++)
-			{
-				DFS(x, y);
-			}
-		}
-
-		
-
-		for (int y = 0; y < N; y++)
-		{
-			for (int x = 0; x < M; x++)
-			{
-				if (vec[y][x] == true)
-				{
-					int ty = parent[y][x].first;
-					int tx = parent[y][x].second;
-					if (ty == -1 && tx == -1)
-					{
-						earthworm++;
-					}
-
-				}
-			}
-		}
-		cout << earthworm << endl;
-	}
+	DIV(0, 0, n, n);
 
 	return 0;
 }
