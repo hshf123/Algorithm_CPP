a203e16f7378d244f9997b655f8ff3189a6c4f14
HanSanghyun <hshf123@naver.com>
2023-01-11 02:08:21 +0900

    1780

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -15,35 +15,69 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
+vector<vector<int>> vec;
+vector<int> _count(3, 0);
+
+void Func(int sx, int ex, int sy, int ey)
+{
+	int firstNum = vec[sy][sx];
+	if (sx == ex - 1)
+	{
+		_count[firstNum + 1]++;
+		return;
+	}
+
+	bool div = false;
+	for (int y = sy; y < ey; y++)
+	{
+		for (int x = sx; x < ex; x++)
+		{
+			if (firstNum != vec[y][x])
+			{
+				div = true;
+				break;
+			}
+		}
+	}
+
+	if (div == false)
+	{
+		_count[firstNum + 1]++;
+		return;
+	}
+
+	int n = (ey - sy) / 3;
+	Func(sx,			sx + n,				sy,				sy + n);
+	Func(sx + n,		sx + (n * 2),		sy,				sy + n);
+	Func(sx + (n * 2),	ex,					sy,				sy + n);
+
+	Func(sx,			sx + n,				sy + n,			sy + (n * 2));
+	Func(sx + n,		sx + (n * 2),		sy + n,			sy + (n * 2));
+	Func(sx + (n * 2),	ex,					sy + n,			sy + (n * 2));
+
+	Func(sx,			sx + n,				sy + (n * 2),			ey);
+	Func(sx + n,		sx + (n * 2),		sy + (n * 2),			ey);
+	Func(sx + (n * 2),	ex,					sy + (n * 2),			ey);
+}
+
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int neverHear, neverSeen;
-	cin >> neverHear >> neverSeen;
+	int N;
+	cin >> N;
 
-	unordered_set<string> hashSet;
-	for (int i = 0; i < neverHear; i++)
-	{
-		string name;
-		cin >> name;
-		hashSet.insert(name);
-	}
+	vec = vector<vector<int>>(N + 1, vector<int>(N + 1));
 
-	set<string> _set;
-	for (int i = 0; i < neverSeen; i++)
-	{
-		string name;
-		cin >> name;
-		auto findIt = hashSet.find(name);
-		if (findIt != hashSet.end())
-			_set.insert(name);
-	}
+	for (int y = 0; y < N; y++)
+		for (int x = 0; x < N; x++)
+			cin >> vec[y][x];
 
-	cout << _set.size() << endl;
-	for (const string& str : _set)
-		cout << str << endl;
+	Func(0, N, 0, N);
+
+	for (int& n : _count)
+		cout << n << endl;
 
 	return 0;
 }
