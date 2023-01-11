813a2d7696c679159c729404417851344fb1d8fb
HanSanghyun <hshf123@naver.com>
2023-01-12 00:44:02 +0900

    2630

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -15,6 +15,54 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
+vector<vector<int>> vec(128, vector<int>(128, -1));
+pair<int, int> _count = make_pair(0, 0);
+
+void Func(int sx, int ex, int sy, int ey)
+{
+	int start = vec[sy][sx];
+	if (ex - sx == 1)
+	{
+		if (start == 0)
+			_count.first++;
+		else
+			_count.second++;
+		return;
+	}
+
+	bool check = true;
+	for (int y = sy; y < ey; y++)
+	{
+		for (int x = sx; x < ex; x++)
+		{
+			if (start != vec[y][x])
+			{
+				check = false;
+				break;
+			}
+		}
+	}
+
+	if (check)
+	{
+		if (start == 0)
+			_count.first++;
+		else
+			_count.second++;
+		return;
+	}
+
+	int n = (ex - sx) / 2;
+
+	Func(sx, sx + n, sy, sy + n);
+
+	Func(sx + n, ex, sy, sy + n);
+
+	Func(sx, sx + n, sy + n, ey);
+
+	Func(sx + n, ex, sy + n, ey);
+}
+
 int main()
 {
 	cin.tie(NULL);
@@ -23,37 +71,17 @@ int main()
 	int N;
 	cin >> N;
 
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
-	for (int i = 0; i < N; i++)
+	for (int y = 0; y < N; y++)
 	{
-		int startTime, endTime;
-		cin >> startTime >> endTime;
-
-		pq.push(make_pair(endTime, startTime));
-	}
-
-	int count = 1;
-	auto copy = pq;
-	pair<int, int> _pair = pq.top();
-	pq.pop();
-	int startTime = _pair.second;
-	int endTime = _pair.first;
-	while (pq.empty() == false)
-	{
-		_pair = pq.top();
-		pq.pop();
-		int nextStartTime = _pair.second;
-		int nextEndTime = _pair.first;
-
-		if (nextStartTime >= endTime)
+		for (int x = 0; x < N; x++)
 		{
-			count++;
-			startTime = nextStartTime;
-			endTime = nextEndTime;
+			cin >> vec[y][x];
 		}
 	}
 
-	cout << count;
+	Func(0, N, 0, N);
+
+	cout << _count.first << endl << _count.second << endl;
 
 	return 0;
 }
