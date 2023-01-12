2609feb63f140a93069ec33707897d81b9f42ea2
HanSanghyun <hshf123@naver.com>
2023-01-12 23:01:53 +0900

    11724

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -15,70 +15,53 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
+int N, M;
+vector<vector<int>> vec;
+vector<bool> visited;
+int _count = 0;
+
+void DFS(int here)
+{
+	visited[here] = true;
+
+	for (int& there : vec[here])
+	{
+		if (visited[there] == false)
+		{
+			visited[there] = true;
+			DFS(there);
+		}
+	}
+}
+
 int main()
 {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	const int all = 0b1111'1111'1111'1111'1111;
-	int now = 0;
+	cin >> N >> M;
 
-	// add remove check toggle all empty
-	int M;
-	cin >> M;
-	string s;
-	getline(cin, s);
+	vec = vector<vector<int>>(N);
+	visited = vector<bool>(N, false);
 	for (int i = 0; i < M; i++)
 	{
-		getline(cin, s);
-		int idx = s.find(' ');
-		int n;
-		if (idx != -1)
-			n = stoi(s.substr(idx, s.length()));
-		s = s.substr(0, idx);
+		int u, v;
+		cin >> u >> v;
 
-		if (s == "add")
+		vec[u - 1].push_back(v - 1);
+		vec[v - 1].push_back(u - 1);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (visited[i] == false)
 		{
-			n = pow(2, n - 1);
-			now |= n;
-		}
-		else if (s == "remove")
-		{
-			n = pow(2, n - 1);
-			n = ~(all & n);
-			now &= n;
-		}
-		else if (s == "check")
-		{
-			n = pow(2, n - 1);
-			if ((now & n) > 0)
-				cout << 1 << endl;
-			else
-				cout << 0 << endl;
-		}
-		else if (s == "toggle")
-		{
-			n = pow(2, n - 1);
-			int toggle = now | n;
-			if (toggle == now)
-			{
-				n = ~(all & n);
-				now &= n;
-			}
-			else
-			{
-				now |= n;
-			}
-		}
-		else if (s == "all")
-		{
-			now |= all;
-		}
-		else if (s == "empty")
-		{
-			now &= 0;
+			_count++;
+			DFS(i);
 		}
 	}
+
+	cout << _count;
 
 	return 0;
 }
