8fe06dd5be0c0f1224f82a7d00ef5313bc3f9326
HanSanghyun <hshf123@naver.com>
2023-01-15 01:13:59 +0900

    5430

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -15,107 +15,95 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
-int N;
-vector<vector<int>> _map;
-vector<int> _linked;
-vector<vector<bool>> _visited;
-int _count = 0;
-
-int ydir[4] = { 1, 0, -1, 0 };
-int xdir[4] = { 0, 1, 0 ,-1 };
-
-bool CheckDir(int y, int x)
-{
-	if (y < 0 || x < 0 || y >= N || x >= N)
-		return false;
-
-	if (_map[y][x] == 0)
-		return false;
-
-	if (_visited[y][x] == true)
-		return false;
-
-	return true;
-}
-
-void BFS(int y, int  x)
-{
-	if (_visited[y][x] == true)
-		return;
-
-	// 현재 위치를 방문 상태로
-	_visited[y][x] = true;
-
-	// 현재 위치는 집이 아니다.
-	if (_map[y][x] == 0)
-		return;
-
-	_count++; // 전체 연결된 단지 수 카운트
-
-	int linked = 1; // 연결된 집의 수 카운트
-
-	queue<pair<int, int>> q;
-	q.push(make_pair(y, x));
-
-	while (q.empty() == false)
-	{
-		pair<int, int> now = q.front();
-		q.pop();
-		int hereY = now.first;
-		int hereX = now.second;
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nextY = hereY + ydir[i];
-			int nextX = hereX + xdir[i];
-
-			if (CheckDir(nextY, nextX))
-			{
-				q.push(make_pair(nextY, nextX));
-				_visited[nextY][nextX] = true;
-				linked++;
-			}
-		}
-	}
-
-	_linked.push_back(linked);
-}
-
 int main()
 {
 	cin.tie(NULL);
+	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	// 1은 집이 있는 곳, 0은 집이 없는 곳
-	// 연결이라 함은 상하좌우로 붙어있는 경우
+	// 두 가지 함수 R(reverse) : 순서 뒤집기 , D(delete) : 첫 번째 수를 버림
 
-	cin >> N;
-
-	_map = vector<vector<int>>(N, vector<int>(N));
-	_visited = vector<vector<bool>>(N, vector<bool>(N, false));
-
-	for (int y = 0; y < N; y++)
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; t++)
 	{
-		string line;
-		cin >> line;
-		for (int x = 0; x < N; x++)
+		string p; // 함수
+		cin >> p;
+		int n; // 배열 개수
+		cin >> n;
+		string arr; // 배열
+		cin >> arr;
+		bool reverse = false; // 뒤집어야 하는거면 true
+		bool isError = false; // 에러인경우
+		deque<int> array;
+		string number = "";
+		for (int i = 1; i < arr.length(); i++)
 		{
-			_map[y][x] = (int)(line[x] - '0');
+			char c = arr[i];
+			if (c == ',' || c == ']')
+			{
+				if(number != "")
+					array.push_back(stoi(number));
+				number = "";
+				continue;
+			}
+
+			number += c;
+		}
+
+		for (int i = 0; i < p.length(); i++)
+		{
+			char c = p[i];
+			if (c == 'R')
+				reverse = !reverse;
+			else
+			{
+				if (array.empty())
+				{
+					isError = true;
+					break;
+				}
+
+				if (reverse)
+					array.pop_back();
+				else
+					array.pop_front();
+			}
+		}
+
+		if (isError)
+			cout << "error" << endl;
+		else
+		{
+			string str;
+			str += '[';
+			if (reverse)
+			{
+				while (array.empty() == false)
+				{
+					int n = array.back();
+					array.pop_back();
+					str += to_string(n);
+					if (array.empty() == false)
+						str += ',';
+				}
+			}
+			else
+			{
+				while (array.empty() == false)
+				{
+					int n = array.front();
+					array.pop_front();
+					str += to_string(n);
+					if (array.empty() == false)
+						str += ',';
+				}
+			}
+			str += ']';
+			cout << str << endl;
 		}
 	}
 
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < N; x++)
-		{
-			BFS(y, x);
-		}
-	}
-
-	cout << _count << endl;
-	sort(_linked.begin(), _linked.end());
-	for (int& n : _linked)
-		cout << n << endl;
 
 	return 0;
 }
