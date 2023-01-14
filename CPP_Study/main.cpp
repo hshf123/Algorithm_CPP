4b64978dd7afd0de5e2813f631585e28c7926d67
HanSanghyun <hshf123@naver.com>
2023-01-14 23:27:58 +0900

    2667

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -15,53 +15,70 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
-int N, M;
+int N;
 vector<vector<int>> _map;
-vector<vector<int>> dist;
-vector<vector<bool>> visited;
-int xdir[4] = { 0, 1, 0, -1 };
-int ydir[4] = { -1, 0, 1, 0 };
+vector<int> _linked;
+vector<vector<bool>> _visited;
+int _count = 0;
 
-bool CheckDir(int nextY, int nextX)
+int ydir[4] = { 1, 0, -1, 0 };
+int xdir[4] = { 0, 1, 0 ,-1 };
+
+bool CheckDir(int y, int x)
 {
-	if (nextY < 0 || nextX < 0 || nextY >= N || nextX >= M)
+	if (y < 0 || x < 0 || y >= N || x >= N)
 		return false;
-	if (visited[nextY][nextX] == true)
+
+	if (_map[y][x] == 0)
 		return false;
-	if (_map[nextY][nextX] == 0)
+
+	if (_visited[y][x] == true)
 		return false;
 
 	return true;
 }
 
-void BFS(int y, int x)
+void BFS(int y, int  x)
 {
-	visited[y][x] = true;
+	if (_visited[y][x] == true)
+		return;
+
+	// 현재 위치를 방문 상태로
+	_visited[y][x] = true;
+
+	// 현재 위치는 집이 아니다.
+	if (_map[y][x] == 0)
+		return;
+
+	_count++; // 전체 연결된 단지 수 카운트
+
+	int linked = 1; // 연결된 집의 수 카운트
 
 	queue<pair<int, int>> q;
 	q.push(make_pair(y, x));
 
 	while (q.empty() == false)
 	{
-		auto here = q.front();
+		pair<int, int> now = q.front();
 		q.pop();
-
-		if (here.first == N - 1 && here.second == M - 1)
-			return;
+		int hereY = now.first;
+		int hereX = now.second;
 
 		for (int i = 0; i < 4; i++)
 		{
-			int nextY = here.first + ydir[i];
-			int nextX = here.second + xdir[i];
+			int nextY = hereY + ydir[i];
+			int nextX = hereX + xdir[i];
 
-			if (CheckDir(nextY, nextX) == true)
+			if (CheckDir(nextY, nextX))
 			{
-				dist[nextY][nextX] = dist[here.first][here.second] + 1;
 				q.push(make_pair(nextY, nextX));
-				visited[nextY][nextX] = true;
+				_visited[nextY][nextX] = true;
+				linked++;
 			}
 		}
 	}
+
+	_linked.push_back(linked);
 }
 
 int main()
@@ -69,23 +86,36 @@ int main()
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> N >> M;
+	// 1은 집이 있는 곳, 0은 집이 없는 곳
+	// 연결이라 함은 상하좌우로 붙어있는 경우
 
-	_map = vector<vector<int>>(N, vector<int>(M));
-	dist = vector<vector<int>>(N, vector<int>(M, 0));
-	visited = vector<vector<bool>>(N, vector<bool>(M, false));
+	cin >> N;
+
+	_map = vector<vector<int>>(N, vector<int>(N));
+	_visited = vector<vector<bool>>(N, vector<bool>(N, false));
+
 	for (int y = 0; y < N; y++)
 	{
 		string line;
 		cin >> line;
-		for (int x = 0; x < M; x++)
+		for (int x = 0; x < N; x++)
 		{
 			_map[y][x] = (int)(line[x] - '0');
 		}
 	}
-		
-	BFS(0, 0);
-	cout << dist[N - 1][M - 1] + 1;
+
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+		{
+			BFS(y, x);
+		}
+	}
+
+	cout << _count << endl;
+	sort(_linked.begin(), _linked.end());
+	for (int& n : _linked)
+		cout << n << endl;
 
 	return 0;
 }
