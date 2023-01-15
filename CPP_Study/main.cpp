03f2a3b4657397dbe82b463324b1d73ad22a4c45
HanSanghyun <hshf123@naver.com>
2023-01-16 03:45:50 +0900

    5525

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -21,89 +21,34 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	// 두 가지 함수 R(reverse) : 순서 뒤집기 , D(delete) : 첫 번째 수를 버림
-
-	int T;
-	cin >> T;
-	for (int t = 0; t < T; t++)
+	int N ,M;
+	cin >> N >> M;
+	string str;
+	cin >> str;
+	
+	int count = 0;
+	int ans = 0;
+	for (int i = 1; i < M - 1;)
 	{
-		string p; // 함수
-		cin >> p;
-		int n; // 배열 개수
-		cin >> n;
-		string arr; // 배열
-		cin >> arr;
-		bool reverse = false; // 뒤집어야 하는거면 true
-		bool isError = false; // 에러인경우
-		deque<int> array;
-		string number = "";
-		for (int i = 1; i < arr.length(); i++)
+		if (str[i] == 'O' && str[i + 1] == 'I')
 		{
-			char c = arr[i];
-			if (c == ',' || c == ']')
+			count++;
+			if (count == N)
 			{
-				if(number != "")
-					array.push_back(stoi(number));
-				number = "";
-				continue;
+				if (str[i - (count * 2 - 1)] == 'I')
+					ans++;
+				count--;
 			}
-
-			number += c;
+			i += 2;
 		}
-
-		for (int i = 0; i < p.length(); i++)
-		{
-			char c = p[i];
-			if (c == 'R')
-				reverse = !reverse;
-			else
-			{
-				if (array.empty())
-				{
-					isError = true;
-					break;
-				}
-
-				if (reverse)
-					array.pop_back();
-				else
-					array.pop_front();
-			}
-		}
-
-		if (isError)
-			cout << "error" << endl;
 		else
 		{
-			string str;
-			str += '[';
-			if (reverse)
-			{
-				while (array.empty() == false)
-				{
-					int n = array.back();
-					array.pop_back();
-					str += to_string(n);
-					if (array.empty() == false)
-						str += ',';
-				}
-			}
-			else
-			{
-				while (array.empty() == false)
-				{
-					int n = array.front();
-					array.pop_front();
-					str += to_string(n);
-					if (array.empty() == false)
-						str += ',';
-				}
-			}
-			str += ']';
-			cout << str << endl;
+			count = 0;
+			i++;
 		}
 	}
 
+	cout << ans;
 
 	return 0;
 }
