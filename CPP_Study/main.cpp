fc1593c92609c14e47e313b2bda0298a7b53222b
HanSanghyun <hshf123@naver.com>
2023-01-18 13:24:41 +0900

    11286

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -16,119 +16,52 @@ using int64 = long long;
 #include <cmath>
 #include <functional>
 
-int N;
-vector<vector<int>> vec;
-vector<vector<int>> cwvec;
-vector<vector<bool>> visited;
-vector<vector<bool>> cwvisited;
-map<int, int> counts;
-map<int, int> cwcounts;
-
-int ydir[4] = { 1,0,0,-1 };
-int xdir[4] = { 0,1,-1,0 };
-
-bool CheckDir(int y, int x,int n)
-{
-	if (y < 0 || y >= N || x < 0 || x >= N)
-		return false;
-
-	return true;
-}
-
-void BFS(int y, int x, vector<vector<int>>& ve, vector<vector<bool>>& vis, map<int,int>& _map)
-{
-	if (vis[y][x] == true)
-		return;
-
-	vis[y][x] = true;
-
-	int n = ve[y][x];
-	auto findIt = _map.find(n);
-	findIt->second++;
-
-	queue<pair<int,int>> qnorm;
-	qnorm.push({ y,x });
-
-	while (qnorm.empty() == false)
-	{
-		auto now = qnorm.front();
-		qnorm.pop();
-		int n = ve[now.first][now.second];
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nextY = now.first + ydir[i];
-			int nextX = now.second + xdir[i];
-
-			if (CheckDir(nextY, nextX, n) && (n == ve[nextY][nextX]) && (vis[nextY][nextX] == false))
-			{
-				qnorm.push({nextY, nextX});
-				vis[nextY][nextX] = true;
-			}
-		}
-	}
-}
-
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
+	int N;
 	cin >> N;
-	vec = vector<vector<int>>(N, vector<int>(N));
-	cwvec = vector<vector<int>>(N, vector<int>(N));
-	visited = vector<vector<bool>>(N, vector<bool>(N, false));
-	cwvisited = vector<vector<bool>>(N, vector<bool>(N, false));
-	for (int y = 0; y < N; y++)
+
+	map<int, priority_queue<int, vector<int>, greater<int>>> _map;
+
+	for (int n = 0; n < N; n++)
 	{
-		string line;
-		cin >> line;
-		for (int x = 0; x < N; x++)
+		int x;
+		cin >> x;
+
+		if (x == 0)
 		{
-			char c = line[x];
-			if (c == 'R')
+			if (_map.empty())
+				cout << 0 << endl;
+			else
 			{
-				vec[y][x] = 0;
-				cwvec[y][x] = 0;
+				auto it = _map.begin();
+				cout << it->second.top() << endl;
+				it->second.pop();
+				if (it->second.empty())
+				{
+					_map.erase(it);
+				}
 			}
-			else if (c == 'G')
+		}
+		else
+		{
+			auto findIt = _map.find(abs(x));
+			if (findIt == _map.end())
 			{
-				vec[y][x] = 1;
-				cwvec[y][x] = 0;
+				priority_queue<int, vector<int>, greater<int>> ms;
+				ms.push(x);
+				_map.insert({ abs(x), ms });
 			}
 			else
 			{
-				vec[y][x] = 2;
-				cwvec[y][x] = 2;
+				findIt->second.push(x);
 			}
 		}
 	}
-
-	counts.insert({ 0, 0 });
-	counts.insert({ 1, 0 });
-	counts.insert({ 2, 0 });
-	cwcounts.insert({ 0, 0 });
-	cwcounts.insert({ 2, 0 });
-
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < N; x++)
-		{
-			BFS(y, x, vec, visited, counts);
-			BFS(y, x, cwvec, cwvisited, cwcounts);
-		}
-	}
-
-	auto it = counts.begin();
-	int count = it++->second;
-	count += it++->second;
-	count += it->second;
-	it = cwcounts.begin();
-	int cwcount = it++->second;
-	cwcount += it->second;
-
-	cout << count << " " << cwcount;
 
 	return 0;
 }
