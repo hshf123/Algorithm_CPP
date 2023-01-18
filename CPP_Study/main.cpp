663f9e66702087051b200022bee6a886154cfe39
HanSanghyun <hshf123@naver.com>
2023-01-18 13:08:07 +0900

    10026

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -14,6 +14,60 @@ using int64 = long long;
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <functional>
+
+int N;
+vector<vector<int>> vec;
+vector<vector<int>> cwvec;
+vector<vector<bool>> visited;
+vector<vector<bool>> cwvisited;
+map<int, int> counts;
+map<int, int> cwcounts;
+
+int ydir[4] = { 1,0,0,-1 };
+int xdir[4] = { 0,1,-1,0 };
+
+bool CheckDir(int y, int x,int n)
+{
+	if (y < 0 || y >= N || x < 0 || x >= N)
+		return false;
+
+	return true;
+}
+
+void BFS(int y, int x, vector<vector<int>>& ve, vector<vector<bool>>& vis, map<int,int>& _map)
+{
+	if (vis[y][x] == true)
+		return;
+
+	vis[y][x] = true;
+
+	int n = ve[y][x];
+	auto findIt = _map.find(n);
+	findIt->second++;
+
+	queue<pair<int,int>> qnorm;
+	qnorm.push({ y,x });
+
+	while (qnorm.empty() == false)
+	{
+		auto now = qnorm.front();
+		qnorm.pop();
+		int n = ve[now.first][now.second];
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nextY = now.first + ydir[i];
+			int nextX = now.second + xdir[i];
+
+			if (CheckDir(nextY, nextX, n) && (n == ve[nextY][nextX]) && (vis[nextY][nextX] == false))
+			{
+				qnorm.push({nextY, nextX});
+				vis[nextY][nextX] = true;
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -21,29 +75,60 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int T;
-	cin >> T;
-	for (int t = 0; t < T; t++)
+	cin >> N;
+	vec = vector<vector<int>>(N, vector<int>(N));
+	cwvec = vector<vector<int>>(N, vector<int>(N));
+	visited = vector<vector<bool>>(N, vector<bool>(N, false));
+	cwvisited = vector<vector<bool>>(N, vector<bool>(N, false));
+	for (int y = 0; y < N; y++)
 	{
-		int N;
-		cin >> N;
-
-		vector<int64> vec(100);
-		vec[0] = 1;
-		vec[1] = 1;
-		vec[2] = 1;
-		vec[3] = 2;
-		vec[4] = 2;
-
-		for (int n = 5; n < N; n++)
+		string line;
+		cin >> line;
+		for (int x = 0; x < N; x++)
 		{
-			vec[n] = vec[n - 2] + vec[n - 3];
-			if (n == N - 1)
-				break;
+			char c = line[x];
+			if (c == 'R')
+			{
+				vec[y][x] = 0;
+				cwvec[y][x] = 0;
+			}
+			else if (c == 'G')
+			{
+				vec[y][x] = 1;
+				cwvec[y][x] = 0;
+			}
+			else
+			{
+				vec[y][x] = 2;
+				cwvec[y][x] = 2;
+			}
 		}
-
-		cout << vec[N - 1] << endl;
 	}
+
+	counts.insert({ 0, 0 });
+	counts.insert({ 1, 0 });
+	counts.insert({ 2, 0 });
+	cwcounts.insert({ 0, 0 });
+	cwcounts.insert({ 2, 0 });
+
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+		{
+			BFS(y, x, vec, visited, counts);
+			BFS(y, x, cwvec, cwvisited, cwcounts);
+		}
+	}
+
+	auto it = counts.begin();
+	int count = it++->second;
+	count += it++->second;
+	count += it->second;
+	it = cwcounts.begin();
+	int cwcount = it++->second;
+	cwcount += it->second;
+
+	cout << count << " " << cwcount;
 
 	return 0;
 }
