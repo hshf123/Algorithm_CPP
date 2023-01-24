a6fa1b426bcc56d2ee51a2e4c465e7c7f7a2cc0d
HanSanghyun <hshf123@naver.com>
2023-01-24 22:41:54 +0900

    11659

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -15,56 +15,6 @@ using int64 = long long;
 #include <algorithm>
 #include <cmath>
 
-int N;
-vector<vector<int>>			_vec;
-map<int, set<int>>			_history;
-vector<int>					_visitedOrder;
-vector<vector<int>>			answer;
-int							_visitedCount;
-vector<bool>				_finished;
-vector<int>					_parent;
-
-void DFS(int here)
-{
-	if (_visitedOrder[here] != 0)
-		return;
-
-	if (_visitedCount != 0)
-		_visitedOrder[here] = 1;
-	_visitedCount++;
-
-	auto findIt = _history.find(here);
-	if (findIt == _history.end())
-	{
-		// 연결된 노드들이 없다.
-		_finished[here] = true;
-		return;
-	}
-
-	// 연결된 노드가 있다.
-	set<int>& next = findIt->second;
-	for (int there : next)
-	{
-		if (_visitedOrder[there] == 0)
-		{
-			_parent[there] = here;
-			DFS(there);
-			continue;
-		}
-
-		if (_visitedOrder[here] < _visitedOrder[there])
-			continue;
-	}
-
-	_finished[here] = true;
-}
-
-void Push(int node, int edge)
-{
-	_vec[node].push_back(edge);
-	set<int>& history = _history[node];
-	history.insert(edge);
-}
 
 int main()
 {
@@ -72,38 +22,37 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> N;
-	_vec.resize(N);
+	int N, M;
+	cin >> N >> M;
 
-	for (int y = 0; y < N; y++)
+	vector<int> vec(N);
+	vector<int> ans(M);
+	int sum = 0;
+	for (int i = 0; i < N; i++)
 	{
-		for (int x = 0; x < N; x++)
+		int n;
+		cin >> n;
+		sum += n;
+		vec[i] = sum;
+	}
+
+	for (int m = 0; m < M; m++)
+	{
+		int i, j;
+		cin >> i >> j;
+
+		if (i == 1)
 		{
-			int n;
-			cin >> n;
-			if (n == 1)
-				Push(y, x);
+			ans[m] = vec[j - 1];
+		}
+		else
+		{
+			ans[m] = vec[j - 1] - vec[i - 2];
 		}
 	}
 
-	for (int y = 0; y < N; y++)
-	{
-		_visitedOrder = vector<int>(N, 0);
-		_visitedCount = 0;
-		_finished = vector<bool>(N, false);
-		_parent = vector<int>(N, -1);
-		DFS(y);
-		answer.push_back(_visitedOrder);
-	}
-
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < N; x++)
-		{
-			cout << answer[y][x] << " ";
-		}
-		cout << endl;
-	}
+	for (int& n : ans)
+		cout << n << endl;
 
 	return 0;
 }
