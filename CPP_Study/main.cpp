92aa0783b6345b7016a0ae06bcb63b115eb55e42
HanSanghyun <hshf123@naver.com>
2023-01-26 00:11:10 +0900

    14500

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -16,26 +16,93 @@ using uint64 = unsigned long long;
 #include <algorithm>
 #include <cmath>
 
+int N, M;
+vector<vector<int>> vec;
+vector<vector<bool>> bools;
+
+int dy[4] = { 1, 0, -1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+
+bool CheckDir(int y, int x)
+{
+	if (y < 0 || x < 0 || y >= N || x >= M)
+		return false;
+
+	return true;
+}
+
+int ans = 0;
+void DFS(int y, int x, int depth, int score)
+{
+	bools[y][x] = true;
+
+	if (depth == 3)
+	{
+		ans = max(ans, score);
+		bools[y][x] = false;
+		return;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		int nextY = y + dy[i];
+		int nextX = x + dx[i];
+		if (CheckDir(nextY, nextX) && bools[nextY][nextX] == false)
+		{
+			bools[nextY][nextX] = true;
+			DFS(nextY, nextX, depth + 1, score + vec[nextY][nextX]);
+			bools[nextY][nextX] = false;
+		}
+	}
+
+	if (x - 1 >= 0 && y - 1 >= 0 && y + 1 < N) 
+	{ //っ
+		ans = max(ans, (vec[y][x - 1] + vec[y][x] + vec[y + 1][x] + vec[y - 1][x]));
+	}
+	if (y - 1 >= 0 && y + 1 < N && x + 1 < M) 
+	{ //た
+		ans = max(ans, (vec[y][x + 1] + vec[y][x] + vec[y + 1][x] + vec[y - 1][x]));
+	}
+	if (x - 1 >= 0 && x + 1 < M && y + 1 < N) 
+	{ //で
+		ans = max(ans, (vec[y + 1][x] + vec[y][x] + vec[y][x - 1] + vec[y][x + 1]));
+	}
+	if (y - 1 >= 0 && x - 1 >= 0 && x + 1 < M) 
+	{ //ぬ
+		ans = max(ans, (vec[y - 1][x] + vec[y][x] + vec[y][x - 1] + vec[y][x + 1]));
+	}
+
+	bools[y][x] = false;
+}
+
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	cin >> N >> M;
 
-	uint64 dp[1001];
+	vec = vector<vector<int>>(N, vector<int>(M));
+	bools = vector<vector<bool>>(N, vector<bool>(M, false));
 
-	dp[1] = 1;
-	dp[2] = 3;
-
-	for (int i = 3; i <= n; i++)
+	for (int y = 0; y < N; y++)
 	{
-		dp[i] = dp[i - 1] + 2 * dp[i - 2];
+		for (int x = 0; x < M; x++)
+		{
+			cin >> vec[y][x];
+		}
 	}
 
-	cout << dp[n] % 10007;
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < M; x++)
+		{
+			DFS(y, x, 0, vec[y][x]);
+		}
+	}
+
+	cout << ans;
 
 	return 0;
 }
