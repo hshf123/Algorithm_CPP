f0e6410a0d2ae2faab4d88d0dbde94c72dd7ed40
HanSanghyun <hshf123@naver.com>
2023-01-29 12:19:36 +0900

    17626

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -16,41 +16,33 @@ using uint64 = unsigned long long;
 #include <algorithm>
 #include <cmath>
 
-
-
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int N, M;
-	cin >> N >> M;
+	int n;
+	cin >> n;
 
-	unordered_map<string, string> siteToPass;
+	vector<int> dp(n + 1);
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 3;
 
-	for (int i = 0; i < N; i++)
+	for (int i = 4; i <= n; i++)
 	{
-		string site;
-		string password;
-		cin >> site >> password;
-		siteToPass.insert({ site, password });
+		int minnum = INT32_MAX;
+		for (int j = 1; j * j <= i; j++)
+		{
+			int idx = i - j * j;
+			minnum = min(dp[idx], minnum);
+		}
+
+		dp[i] = minnum + 1;
 	}
 
-	vector<string> ans(M);
-	for (int i = 0; i < M; i++)
-	{
-		string site;
-		cin >> site;
-
-		auto it = siteToPass.find(site);
-		ans[i] = it->second;
-	}
-
-	for (string& str : ans)
-	{
-		cout << str << endl;
-	}
+	cout << dp[n];
 
 	return 0;
 }
