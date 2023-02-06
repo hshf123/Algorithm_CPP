78feb462afcc0d81d6dad194514a7a400c1e0b9b
HanSanghyun <hshf123@naver.com>
2023-02-06 22:32:10 +0900

    1504

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define endl "\n"
 using int64 = long long;
 using uint64 = unsigned long long;
+#define INT32_HALF (2147483647 / 2)
 
 #include <vector>
 #include <list>
@@ -16,20 +17,20 @@ using uint64 = unsigned long long;
 #include <algorithm>
 #include <cmath>
 
-struct PQNode
+struct Node
 {
-	bool operator<(const PQNode& other) const { return time < other.time; }
-	bool operator>(const PQNode& other) const { return time > other.time; }
+	bool operator<(const Node& other) const { return cost < other.cost; }
+	bool operator>(const Node& other) const { return cost > other.cost; }
 
-	int	pos;
-	int	time;
+	int	edge;
+	int	cost;
 };
 
-int N, M, X;
-vector<vector<PQNode>> roads;
-int path = 0;
+int N, E;
+int v1, v2;
+vector<vector<Node>> nodes;
 
-void AStar(int start,int cost, int dest)
+int AStar(int start, int dest, int cost = 0)
 {
 	// 점수 매기기
 	// F = G + H
@@ -42,10 +43,10 @@ void AStar(int start,int cost, int dest)
 	vector<bool> closed(N + 1, false);
 
 	// best[y][x] -> 지금까지 (y, x)에 대한 가장 좋은 비용 (작을 수록 좋음)
-	vector<int> best(N + 1, INT32_MAX);
+	vector<int> best(N + 1, INT32_HALF);
 
 	// OpenList
-	priority_queue<PQNode, vector<PQNode>, greater<PQNode>> pq;
+	priority_queue<Node, vector<Node>, greater<Node>> pq;
 
 	// 1) 예약(발견) 시스템 구현
 	// - 이미 더 좋은 경로를 찾았다면 스킵
@@ -54,54 +55,59 @@ void AStar(int start,int cost, int dest)
 	// - pq에서 pop한 다음에 무시한다거나.
 
 	// 초기값
+	int path = -1;
 	{
 		int g = 0;
-		pq.push(PQNode{ start, cost });
+		pq.push(Node{ start, cost });
 		best[start] = cost;
 	}
 
 	while (pq.empty() == false)
 	{
 		// 제일 좋은 후보를 찾는다
-		PQNode node = pq.top();
+		Node node = pq.top();
 		pq.pop();
 
 		// 동일한 좌표를 여러 경로로 찾아서\
 		// 더 빠른 경로로 인해서 이미 방문(closed)된 경우 스킵
 		// [선택]
-		if (closed[node.pos])
+		if (closed[node.edge])
 			continue;
-		if (best[node.pos] < node.time)
+		if (best[node.edge] < node.cost)
 			continue;
 
 		// 방문
-		closed[node.pos] = true;
+		closed[node.edge] = true;
 
 		// 목적지에 도착했으면 바로 종료
-		if (node.pos == dest)
+		if (node.edge == dest)
 		{
+			if (path == -1)
+				path++;
 			path += best[dest];
 			break;
 		}
 
-		for (auto& next : roads[node.pos])
+		for (auto& next : nodes[node.edge])
 		{
-			int nextPos = next.pos;
+			int nextPos = next.edge;
 			// [선택] 이미 방문한 곳이면 스킵
 			if (closed[nextPos])
 				continue;
 
 			// 비용 계산
-			int time = best[node.pos] + next.time;
+			int time = best[node.edge] + next.cost;
 			// 다른 경로에서 더 빠른 길을 찾았으면 스킵
 			if (best[nextPos] <= time)
 				continue;
 
 			// 예약 진행
 			best[nextPos] = time;
-			pq.push(PQNode{ nextPos,time });
+			pq.push(Node{ nextPos,time });
 		}
 	}
+
+	return path;
 }
 
 int main()
@@ -110,27 +116,35 @@ int main()
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> N >> M >> X;
-	roads = vector<vector<PQNode>>(N + 1);
+	cin >> N >> E;
+	nodes = vector<vector<Node>>(N + 1);
 
-	for (int i = 1; i <= M; i++)
+	for (int i = 0; i < E; i++)
 	{
-		int start, end, time;
-		cin >> start >> end >> time;
+		int a, b, c;
+		cin >> a >> b >> c;
 
-		roads[start].push_back({ end, time });
-	}
-	
-	int _max = 0;
-	for (int i = 1; i <= N; i++)
-	{
-		path = 0;
-		AStar(i, 0, X);
-		AStar(X, 0, i);
-		_max = max(_max, path);
+		nodes[a].push_back({ b,c });
+		nodes[b].push_back({ a,c });
 	}
 
-	cout << _max;
+	cin >> v1 >> v2;
+
+	int res1, res2;
+	int a1 = AStar(1, v1);
+	int a2 = AStar(v1, v2);
+	int a3 = AStar(v2, N);
+	if (a1 == -1 || a2 == -1 || a3 == -1) res1 = -1;
+	else res1 = a1 + a2 + a3;
+
+	int b1 = AStar(1, v2);
+	int b2 = AStar(v2, v1);
+	int b3 = AStar(v1, N);
+	if (b1 == -1 || b2 == -1 || b3 == -1) res2 = -1;
+	else res2 = b1 + b2 + b3;
+
+	if (res1 == -1 && res2 == -1) cout << -1;
+	else cout << min(res1, res2);
 
 	return 0;
 }
