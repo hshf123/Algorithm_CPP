ac1095430c9ebfdef0fbd03cca1c22420574f4b6
HanSanghyun <hshf123@naver.com>
2023-02-14 21:14:26 +0900

    15650

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -16,100 +16,40 @@
 using namespace std;
 using int64 = long long;
 using uint64 = unsigned long long;
-using BYTE = unsigned char;
 
-vector<string> cache[101];
+int N, M;
+vector<int> seq;
+vector<bool> visited;
 
-string Add(string num1, string num2)
+void BackTracking(int num, int cnt)
 {
-	string res = "";
-	
-	if (num1.size() < num2.size())
+	if (cnt == M)
 	{
-		string temp = num1;
-		num1 = num2;
-		num2 = temp;
+		for (int i = 0; i < M; i++)
+			cout << seq[i] << ' ';
+		cout << '\n';
+		return;
 	}
-
-	int num1Iter = num1.size() - 1;
-	int num2Iter = num2.size() - 1;
-	bool up = false;
-	for (int i = num2Iter; i >= 0; --i)
+	for (int i = num; i <= N; i++)
 	{
-		int n1 = num1[num1Iter] - '0';
-		int n2 = num2[num2Iter] - '0';
-
-		int n = n1 + n2;
-		if (up)
-			n++;
-		if (n >= 10)
+		if (!visited[i])
 		{
-			up = true;
-			n -= 10;
+			visited[i] = true;
+			seq[cnt] = i;
+			BackTracking(i + 1, cnt + 1);
+			visited[i] = false;
 		}
-		else
-			up = false;
-
-		char c = n + '0';
-		res += c;
-
-		num1Iter--;
-		num2Iter--;
 	}
-
-	for (int i = num1Iter; i >= 0; --i)
-	{
-		int n = num1[i] - '0';
-
-		if (up)
-			n++;
-
-		if (n >= 10)
-		{
-			up = true;
-			n -= 10;
-		}
-		else
-			up = false;
-
-		char c = n + '0';
-		res += c;
-	}
-
-	if (up)
-		res += "1";
-
-	::reverse(res.begin(), res.end());
-
-	return res;
-}
-
-string Combination(string n, string m)
-{
-	if (m == "0" || m == n)
-		return "1";
-
-	string& ret = cache[stoi(n)][stoi(m)];
-	if (ret != "-1")
-		return ret;
-
-	string n_1 = to_string(stoi(n) - 1);
-	string m_1 = to_string(stoi(m) - 1);
-
-	return ret = Add(Combination(n_1, m_1), Combination(n_1, m));
 }
 
 int main()
 {
 	Init;
 
-	for (int i = 0; i < 101; i++)
-		cache[i] = vector<string>(101, "-1");
-
-	string n, m;
-	cin >> n >> m;
-
-	cout << Combination(n, m);
+	cin >> N >> M;
+	seq = vector<int>(9, 0);
+	visited = vector<bool>(9, false);
+	BackTracking(1, 0);
 
 	return 0;
 }
