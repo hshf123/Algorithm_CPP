868137ea115a3ec7930eab619d70e4c53cfc37bd
HanSanghyun <hshf123@naver.com>
2023-02-23 19:31:58 +0900

    11660

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -17,50 +17,64 @@ using namespace std;
 using int64 = long long;
 using uint64 = unsigned long long;
 
-int dx[] = { 0,1,0,-1 };
-int dy[] = { 1,0,-1,0 };
-int n;
-
-vector<vector<int>> stickers;
+int N, M;
+vector<vector<int>> board;
 vector<vector<int>> cache;
 
-int DP()
+int SectionSum(int y, int x)
 {
-	cache[0][0] = stickers[0][0];
-	cache[1][1] = stickers[1][1] + cache[0][0];
+	if (y < 0 || x < 0)
+		return 0;
 
-	cache[1][0] = stickers[1][0];
-	cache[0][1] = stickers[0][1] + cache[1][0];
+	int& ret = cache[y][x];
+	if (ret != -1)
+		return ret;
 
-	for (int i = 2; i < n; i++) {
-		cache[0][i] = stickers[0][i] + max(cache[1][i - 1], cache[1][i - 2]);
-		cache[1][i] = stickers[1][i] + max(cache[0][i - 1], cache[0][i - 2]);
+	int sum = 0;
+	int yi = 0;
+	int xi = 0;
+	while (yi <= y)
+	{
+		sum += board[yi][xi];
+
+		if (yi == 0)
+			cache[yi][xi] = sum;
+
+		if (yi > 0)
+			cache[yi][xi] = sum - cache[yi - 1][x] + cache[yi - 1][xi];
+
+		xi++;
+		if (xi > x)
+		{
+			cache[yi][xi - 1] = sum;
+
+			xi = 0;
+			yi++;
+		}
 	}
 
-	return max(cache[0][n - 1], cache[1][n - 1]);
+	return ret = sum;
 }
 
 int main()
 {
 	Init;
 
-	int T;
-	cin >> T;
-
-	for (int i = 0; i < T; i++)
+	cin >> N >> M;
+	board = vector<vector<int>>(N, vector<int>(N));
+	cache = vector<vector<int>>(N, vector<int>(N, -1));
+	for (int y = 0; y < N; y++)
 	{
-		cin >> n;
-		stickers = vector<vector<int>>(2, vector<int>(n));
-		cache = vector<vector<int>>(2, vector<int>(n, -1));
-		for (int k = 0; k < 2; k++)
+		for (int x = 0; x < N; x++)
 		{
-			for (int j = 0; j < n; j++)
-			{
-				cin >> stickers[k][j];
-			}
+			cin >> board[y][x];
 		}
-
-		cout << DP() << endl;
+	}
+	for (int i = 0; i < M; i++)
+	{
+		int x1, y1, x2, y2;
+		cin >> y1 >> x1 >> y2 >> x2;
+		cout << SectionSum(y2 - 1, x2 - 1) - SectionSum(y2 - 1, x1 - 2) - SectionSum(y1 - 2, x2 - 1) + SectionSum(y1 - 2, x1 - 2) << endl;
 	}
 	
 	return 0;
