5496a5d9068677a5725330a69c748f9960876561
HanSanghyun <hshf123@naver.com>
2023-03-02 23:09:02 +0900

    1918

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -18,87 +18,69 @@ using namespace std;
 using int64 = long long;
 using uint64 = unsigned long long;
 
-struct Pos
+void PostOrder(string& str)
 {
-	int y, x, crash;
-};
+	stack<char> s;
+	auto it = str.begin();
 
-int dx[] = { 0, 1, 0, -1 };
-int dy[] = { -1, 0, 1, 0 };
-int visited[1000][1000][2];
-
-int N, M;
-vector<vector<int>> matrix;
-
-bool CheckDir(int y, int x)
-{
-	if (y < 0 || x < 0 || y >= N || x >= M)
-		return false;
-
-	return true;
-}
-
-int BFS(int y, int x)
-{
-	queue<Pos> q;
-	q.push({ y,x,1 });
-	visited[y][x][1] = true;
-
-	while (q.empty() == false)
+	while (it != str.end())
 	{
-		int nowY = q.front().y;
-		int nowX = q.front().x;
-		int crash = q.front().crash;
+		char now = *it;
 
-		q.pop();
-
-		if (nowY == N - 1 && nowX == M - 1)
+		if (now == '(')
 		{
-			return visited[nowY][nowX][crash];
+			s.push(now);
 		}
-
-		for (int i = 0; i < 4; i++)
+		else if (now == '*' || now == '/')
 		{
-			int nextY = nowY + dy[i];
-			int nextX = nowX + dx[i];
-
-			if (CheckDir(nextY, nextX))
+			while (s.empty() == false && (s.top() == '/' || s.top() == '*'))
 			{
-				if (matrix[nextY][nextX] == 1 && crash == 1)
-				{
-					q.push({ nextY, nextX, 0 });
-					visited[nextY][nextX][0] = visited[nowY][nowX][crash] + 1;
-				}
-				else if (matrix[nextY][nextX] == 0 && visited[nextY][nextX][crash] == 0)
-				{
-					q.push({ nextY, nextX, crash });
-					visited[nextY][nextX][crash] = visited[nowY][nowX][crash] + 1;
-				}
+				cout << s.top();
+				s.pop();
 			}
+			s.push(now);
 		}
+		else if (now == '+' || now == '-')
+		{
+			while (s.empty() == false && s.top() != '(')
+			{
+				cout << s.top();
+				s.pop();
+			}
+			s.push(now);
+		}
+		else if (now == ')')
+		{
+			while (s.top() != '(')
+			{
+				cout << s.top();
+				s.pop();
+			}
+			s.pop();
+		}
+		else
+		{
+			cout << now;
+		}
+
+		++it;
 	}
 
-	return -1;
+	while (s.empty() == false)
+	{
+		cout << s.top();
+		s.pop();
+	}
 }
 
 int main()
 {
 	Init;
 
-	cin >> N >> M;
-	matrix = vector<vector<int>>(N, vector<int>(M));
-	for (int y = 0; y < N; y++)
-	{
-		string num;
-		cin >> num;
-		for (int x = 0; x < M; x++)
-		{
-			matrix[y][x] = num[x] - '0';
-		}
-	}
+	string str = "A+(B*C)-(D/E)";
+	//cin >> str;
 
-	
-	cout << BFS(0, 0);
+	PostOrder(str);
 
 	return 0;
 }
