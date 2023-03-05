a04baabd8d49695708e91ff0e204effa1135c28f
HanSanghyun <hshf123@naver.com>
2023-03-06 02:13:16 +0900

    15686

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -19,50 +19,130 @@ using namespace std;
 using int64 = long long;
 using uint64 = unsigned long long;
 
-struct Node
-{
-	Node* left;
-	int key;
-	Node* right;
-};
+int N, M;
+vector<vector<int>> _map;
+vector<pair<int, int>> city;
 
-void Insert(Node*& root, int value)
+vector<pair<int, int>> chicken;
+vector<pair<int, int>> output;
+vector<bool> visited;
+
+int dy[] = { 1,0,-1,0 };
+int dx[] = { 0,1,0,-1 };
+
+int ans = INT32_MAX;
+
+bool CheckDir(int y, int x)
 {
-	if (root == nullptr)
+	if (y < 0 || x < 0 || y >= N || x >= N)
+		return false;
+	return true;
+}
+
+int FindChickenPath2(int y, int x)
+{
+	int ret = INT32_MAX;
+	for (pair<int, int> p : output)
 	{
-		root = new Node();
-		root->key = value;
+		int cy = p.first;
+		int cx = p.second;
+
+		ret = min(ret, abs(y - cy) + abs(x - cx));
+	}
+
+	return ret;
+}
+
+int FindChickenPath(int y, int x)
+{
+	bool mapVisited[50][50];
+	::memset(mapVisited, false, sizeof(mapVisited));
+	priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
+	q.push({ 0, {y,x} });
+	mapVisited[y][x] = true;
+
+	while (q.empty() == false)
+	{
+		int dist = q.top().first;
+		int nowY = q.top().second.first;
+		int nowX = q.top().second.second;
+		q.pop();
+
+		for (pair<int,int> p : output)
+		{
+			int cy = p.first;
+			int cx = p.second;
+
+			if (cy == nowY && cx == nowX)
+			{
+				return dist;
+			}
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nextY = nowY + dy[i];
+			int nextX = nowX + dx[i];
+
+			if (CheckDir(nextY, nextX) && mapVisited[nextY][nextX] == false)
+			{
+				mapVisited[nextY][nextX] = true;
+				q.push({ dist + 1, {nextY, nextX} });
+			}
+		}
+	}
+}
+
+void BackTracking(int n, int count)
+{
+	if (count == M)
+	{
+		int sum = 0;
+		for (pair<int, int> p : city)
+		{
+			int y = p.first;
+			int x = p.second;
+			sum += FindChickenPath2(y, x);
+		}
+		ans = min(ans, sum);
 		return;
 	}
 
-	if (root->key > value)
-		Insert(root->left, value);
-	else
-		Insert(root->right, value);
-}
-
-void PrintPostOrder(Node* node)
-{
-	if (node == nullptr)
-		return;
-
-	PrintPostOrder(node->left);
-	PrintPostOrder(node->right);
-	cout << node->key << endl;
+	for (int i = n; i < chicken.size(); i++)
+	{
+		if (visited[i] == false)
+		{
+			visited[i] = true;
+			output[count] = chicken[i];
+			BackTracking(i, count + 1);
+			visited[i] = false;
+		}
+	}
 }
 
 int main()
 {
 	Init;
 
-	Node* root = nullptr;
-
-	int n;
-	while (cin >> n)
+	cin >> N >> M;
+	_map = vector<vector<int>>(N, vector<int>(N));
+	output = vector<pair<int, int>>(M);
+	for (int y = 0; y < N; y++)
 	{
-		Insert(root, n);
+		for (int x = 0; x < N; x++)
+		{
+			cin >> _map[y][x];
+			if (_map[y][x] == 2)
+				chicken.push_back({ y,x });
+			else if (_map[y][x] == 1)
+				city.push_back({ y,x });
+		}
 	}
-	PrintPostOrder(root);
+
+	visited = vector<bool>(chicken.size(), false);
+	BackTracking(0, 0);
+
+	cout << ans;
 
 	return 0;
 }
