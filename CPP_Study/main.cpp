95332ac9a57ded6e1cf082041001018dbd976122
HanSanghyun <hshf123@naver.com>
2023-05-19 17:22:37 +0900

    vector 구현 연습

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -12,6 +12,7 @@
 #include <cmath>
 #include <cstring>
 #include <numeric>
+#include <sstream>
 
 #define endl "\n"
 #define INT32_HALF (INT32_MAX / 2)
@@ -20,39 +21,81 @@ using namespace std;
 using int64 = long long;
 using uint64 = unsigned long long;
 
-int64 M;
-const int64 MOD = 1'000'000'007;
-
-int64 f(int64 x, int64 y)
+template<class T>
+class Vector
 {
-	if (y == 1) 
-		return x;
-	if (y % 2 == 1) 
-		return x * f(x, y - 1) % MOD;
-	int64 p = f(x, y / 2);
-	return p * p % MOD;
-}
+public:
+	void reserve(int capacity)
+	{
+		if (_capacity > capacity)
+			return;
+
+		T* array = new T[capacity];
+		for (int i = 0; i < _size; i++)
+		{
+			array[i] = _array[i];
+		}
+		_array = array;
+		_capacity = capacity;
+	}
+	void resize(int size)
+	{
+		reserve(size);
+		for (int i = _size; i < size; i++)
+		{
+			_array[i] = T();
+		}
+		_size = size;
+	}
+	void push_back(T data)
+	{
+		if (_size >= _capacity)
+		{
+			if (_size == _size * 1.5)
+				_capacity++;
+			else
+				_capacity *= 1.5;
+
+			reserve(_capacity);
+		}
+
+		_array[_size++] = data;
+	}
+
+public:
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+	T back() { return _array[_size - 1]; }
+	T& operator[](int idx) { return *(_array + idx); }
+
+private:
+	T* _array;
+	int _size = 0;
+	int _capacity = 0;
+};
 
 int main()
 {
-	Init;
+	Vector<int> vec;
+	cout << "1" << endl;
+	vec.push_back(1);
+	cout << vec.size() << endl;
+	cout << vec.capacity() << endl;
+	cout << endl;
+	cout << "2" << endl;
+	vec.reserve(3);
+	cout << vec.size() << endl;
+	cout << vec.capacity() << endl;
+	cout << endl;
+	cout << "3" << endl;
+	vec.resize(5);
+	cout << vec.size() << endl;
+	cout << vec.capacity() << endl;
+	cout << endl;
+	cout << vec[0] << endl;
+	vec[0] = 3;
+	cout << vec[0] << endl;
 
-	cin >> M;
-	int64 ans = 0;
-	for (int i = 0; i < M; i++)
-	{
-		int64 N, S;
-		cin >> N >> S;
-
-		int64 g = ::gcd(N, S);
-
-		N /= g;
-		S /= g;
-		ans += S * f(N, MOD - 2) % MOD;
-		ans %= MOD;
-	}
-
-	cout << ans;
 
 	return 0;
 }
