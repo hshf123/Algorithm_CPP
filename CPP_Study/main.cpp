2879a82fb0955cbca3d80f6c5d4926d871e65ef2
HanSanghyun <hshf123@naver.com>
2023-05-19 17:27:15 +0900

    queue 구현 연습

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -21,80 +21,46 @@ using namespace std;
 using int64 = long long;
 using uint64 = unsigned long long;
 
-template<class T>
-class Vector
+template<class T, class Container = deque<T>>
+class Queue
 {
 public:
-	void reserve(int capacity)
+	void push(T data)
 	{
-		if (_capacity > capacity)
-			return;
-
-		T* array = new T[capacity];
-		for (int i = 0; i < _size; i++)
-		{
-			array[i] = _array[i];
-		}
-		_array = array;
-		_capacity = capacity;
+		container.push_back(data);
 	}
-	void resize(int size)
+	T front()
 	{
-		reserve(size);
-		for (int i = _size; i < size; i++)
-		{
-			_array[i] = T();
-		}
-		_size = size;
-	}
-	void push_back(T data)
-	{
-		if (_size >= _capacity)
-		{
-			if (_size == _size * 1.5)
-				_capacity++;
-			else
-				_capacity *= 1.5;
-
-			reserve(_capacity);
-		}
-
-		_array[_size++] = data;
+		return container.front();
 	}
 
-public:
-	int size() { return _size; }
-	int capacity() { return _capacity; }
-	T back() { return _array[_size - 1]; }
-	T& operator[](int idx) { return *(_array + idx); }
+	bool empty()
+	{
+		return container.empty();
+	}
+	void pop()
+	{
+		container.pop_front();
+	}
+
 
 private:
-	T* _array;
-	int _size = 0;
-	int _capacity = 0;
+	Container container;
 };
 
 int main()
 {
-	Vector<int> vec;
-	cout << "1" << endl;
-	vec.push_back(1);
-	cout << vec.size() << endl;
-	cout << vec.capacity() << endl;
-	cout << endl;
-	cout << "2" << endl;
-	vec.reserve(3);
-	cout << vec.size() << endl;
-	cout << vec.capacity() << endl;
-	cout << endl;
-	cout << "3" << endl;
-	vec.resize(5);
-	cout << vec.size() << endl;
-	cout << vec.capacity() << endl;
-	cout << endl;
-	cout << vec[0] << endl;
-	vec[0] = 3;
-	cout << vec[0] << endl;
+	Queue<int> q;
+	for (int i = 0; i < 100; i++)
+	{
+		q.push(i + 1);
+	}
+
+	while (q.empty() == false)
+	{
+		cout << q.front() << endl;
+		q.pop();
+	}
 
 
 	return 0;
