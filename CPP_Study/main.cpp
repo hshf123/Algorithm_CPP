28465adabb8bdd51f72b54bbb71b6b0daf678f8d
Sanghyun <hshf123@fowgames.com>
2024-06-24 21:44:42 +0900

    1987 - 알파벳
    STL 써서 시간 오래걸렸다고 틀린듯;

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,45 +24,65 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-int N;
-vector<int> nums;
-vector<int> arr;
+int R, C;
+vector<vector<char>> vec;
+vector<vector<bool>> discovered;
 
-int twoPointer(int left, int right, int cnt, int kind, int m)
+array<pair<int, int>, 4> dir =
 {
-	if (right >= N)
-		return m;
+	make_pair(0, 1),
+	make_pair(1, 0),
+	make_pair(0, -1),
+	make_pair(-1, 0)
+};
 
-	if (nums[arr[right]] == 0)
-		kind++;
+int ans = 0;
 
-	cnt++;
-	nums[arr[right]]++;
+bool CanGo(const int& y, const int& x)
+{
+	if (y < 0 || x < 0 || y >= R || x >= C)
+		return false;
 
-	// 만약 과일의 종류가 2개를 넘으면, 왼쪽의 포인터를 이동한다.
-	if (kind > 2)
+	return true;
+}
+
+void DFS(const int& y, const int& x, unordered_set<char>& log, int count)
+{
+	ans = max(ans, count);
+
+	for (auto& p : dir)
 	{
-		if (--nums[arr[left]] == 0)
-			kind--;
-		cnt--;
-		left++;
-	}
+		int nextY = p.first + y;
+		int nextX = p.second + x;
+		if (CanGo(nextY, nextX) == false || discovered[nextY][nextX] || log.find(vec[nextY][nextX]) != log.end())
+			continue;
 
-	m = max(m, cnt);
-	return twoPointer(left, right + 1, cnt, kind, m);
+		log.insert(vec[nextY][nextX]);
+		discovered[nextY][nextX] = true;
+		DFS(nextY, nextX, log, count + 1);
+		log.erase(vec[nextY][nextX]);
+		discovered[nextY][nextX] = false;
+	}
 }
 
 int main()
 {
 	Init;
-	cin >> N;
-	nums.resize(10, 0);
-	for (int i = 0; i < N; i++)
+	cin >> R >> C;
+	vec.resize(R, vector<char>(C));
+	discovered.resize(R, vector<bool>(C, false));
+	for (int y = 0; y < R; y++)
 	{
-		int t;
-		cin >> t;
-		arr.push_back(t);
+		string str;
+		cin >> str;
+		for (int x = 0; x < C; x++)
+		{
+			vec[y][x] = str[x];
+		}
 	}
-
-	cout << twoPointer(0, 0, 0, 0, 0);
+	unordered_set<char> log;
+	discovered[0][0] = true;
+	log.insert(vec[0][0]);
+	DFS(0, 0, log, 1);
+	cout << ans;
 }
