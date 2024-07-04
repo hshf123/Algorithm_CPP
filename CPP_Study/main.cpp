f5d21aa10f67e3d3d938d02798d53b649f5a0a86
Sanghyun <hshf123@fowgames.com>
2024-07-04 22:42:02 +0900

    그라나도에스파다 - 코테 3번

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -25,58 +25,48 @@ using uint64 = unsigned long long;
 #pragma endregion
 
 /*
-선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다.
-예를들어 선행 스킬 순서가 (스파크 -> 라이트닝볼트 -> 썬더) 일 때,
-썬더를 배우려면 먼저 라이트닝 볼트를 배워야하고, 라이트닝 볼트를 배우려면 먼저 스파크를 배워야 합니다.
-
-위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 따라서 (스파크 -> 힐링 -> 라이트닝볼트 -> 썬더)와
-같은 스킬트리는 가능하지만, (썬더 -> 스파크)나 (라이트닝볼트 -> 스파크 -> 힐링 -> 썬더) 와 같은 스킬트리는 불가능합니다.
-
-선행 스킬 순서 skill과 유저들이 만든 스킬트리를 담은 배열 skill_trees가 매개변수로 주어질 때, 가능한 스킬트리 개수를 return하는 프로그램을 작성해주세요.
-
---------------------------------
-			제한조건
---------------------------------
-스킬은 알파벳 대문자로 표기하며, 모든 문자열은 알파벳 대문자로만 이루어져 있습니다.
-스킬 순서와 스킬트리는 문자열로 표기합니다. - 예를들어 (C -> B -> D)라면 "CBD"로 표기합니다.
-선행 스킬 순서 skill의 길이는 1이상 26이하이며, 스킬은 중복해 주어지지 않습니다.
-skill_trees는 길이 1이상 20이하인 배열입니다.
-skill_trees의 원소는 스킬을 나타내는 문자열입니다.
-skill_trees의 원소는 길이가 2이상 26이하인 문자열이며, 스킬이 중복해 주어지지 않습니다.
+금 한 돈의 값은 매일 변합니다. 금값이 낮을 때 금을 사고, 높을 때 팔면 이익을 낼 수 있습니다. 다음 규칙에 맞게 금을 사고 팔아 이익을 남기려고 합니다.
+1. 금은 여러 번 사고 팔 수 있습니다.
+2. 금을 살 때는 한 돈만 살 수 있으며 팔 때도 한 돈만 팔 수 있습니다.
+3. 금은 최대 한돈 까지만 가지고 있을 수 있습니다. (즉, 현재 가지고 있는 금을 팔기 전까지는 금을 살 수 없습니다.)
+4. 금을 판 날과 다음날에는 금을 사지 않습니다.
+5. 이익이 발생하지 않는다면 금을 사고팔지 않아도 됩니다.
+i번째 요소가 i번째 일의 금 한 돈의 값인 배열 gold_prices가 매개변수로 주어질 때, 금을 사고팔아 얻을 수 있는 최대 이윤을 출력하는 프로그램을 작성해주세요.
+제한사항
+1. 배열 gold_prices의 크기 : 100,000 이하의 자연수
+2. 금값 (배열의 원소) : 10,000 이하의 자연수
 */
 
+int GoldPrice(const vector<int>& gold_prices)
+{
+	vector<int> buy(gold_prices.size(), 0);		// n번째 날 살 때 이윤
+	vector<int> sell(gold_prices.size(), 0);	// n번째 날 팔 때 이윤
+
+	// 첫날 살 때와 팔 때의 이윤
+	buy[0] = -gold_prices[0];
+	sell[0] = 0;
+	// 둘째 날 
+	buy[1] = max(buy[0], -gold_prices[1]);				// 어제 산게 더 나은건지 오늘산게 더 나은건지
+	sell[1] = max(sell[0], buy[0] + gold_prices[1]);	// 어제 사서 오늘 파는게 이득임?
+	for (int i = 2; i < gold_prices.size(); i++)
+	{
+		buy[i] = max(buy[i - 1], sell[i - 2] - gold_prices[i]);
+		sell[i] = max(sell[i - 1], buy[i - 1] + gold_prices[i]);
+	}
+
+	return sell[gold_prices.size() - 1];
+}
 
 int main()
 {
 	Init;
 
-	// 입력 예제 Ans)2
-	string skill = "CBD";
-	string skill_trees[] = { "BACDE", "CBADF", "AECB", "BDA" };
+	// 입력 예제1 Ans)4
+	vector<int> gold_prices1 = { 2,5,1,3,4 };
+	vector<int> gold_prices2 = { 7,2,5,6,1,4,2,8 };
 
-	unordered_set<char> skillus;
-	vector<char> skillVec;
-	for (char c : skill)
-	{
-		skillus.insert(c);
-		skillVec.push_back(c);
-	}
-
-	int ans = 0;
-	for (string& str : skill_trees)
-	{
-		string sk = "";
-		for (char& c : str)
-		{
-			auto findIt = skillus.find(c);
-			if (findIt != skillus.end())
-				sk += c;
-		}
-		if (sk.empty() || skill == sk || (sk.size() != skill.size() && skill.substr(0, sk.size()) == sk))
-			ans++;
-	}
-
-	cout << ans;
+	cout << GoldPrice(gold_prices1) << endl;
+	cout << GoldPrice(gold_prices2) << endl;
 
 	return 0;
 }
