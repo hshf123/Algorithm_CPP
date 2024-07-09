65d5ac5328327b3733f02edf0c8b99729e5c80b3
Sanghyun <hshf123@fowgames.com>
2024-07-09 21:44:39 +0900

    1647 - 도시 분할 계획

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,56 +24,94 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-vector<int> GetPrimeNumberList(const int& number)
+class DisjointSet
 {
-	vector<int> ret;
-	vector<bool> vec(number + 1, false);
-	for (int i = 2; i <= number; i++)
+public:
+	DisjointSet(const int& nodeCount) : _parent(nodeCount), _rank(nodeCount, 1)
 	{
-		if (vec[i])
-			continue;
-		ret.push_back(i);
-		for (int idx = i; idx <= number; idx += i)
-			vec[idx] = true;
+		for (int i = 0; i < nodeCount; i++)
+			_parent[i] = i;
 	}
 
-	return ret;
-}
+	int Find(int u)
+	{
+		while (true)
+		{
+			if (u == _parent[u])
+				return u;
+
+			u = _parent[u];
+		}
+	}
+
+	void Merge(int u, int v)
+	{
+		u = Find(u);
+		v = Find(v);
+
+		if (u == v)
+			return;
+
+		if (_rank[u] > _rank[v])
+			::swap(u, v);
+
+		_parent[u] = v;
+		if (_rank[u] == _rank[v])
+			_rank[v]++;
+	}
+
+private:
+	vector<int> _parent;
+	vector<int> _rank;
+};
+
+struct CostEdge
+{
+	int cost;
+	int u;
+	int v;
+	bool operator<(const CostEdge& other)
+	{
+		return cost < other.cost;
+	}
+};
 
 int main()
 {
 	Init;
 
-	int N;
-	cin >> N;
-	vector<int> primeNumberList = GetPrimeNumberList(N);
+	int N, M;
+	cin >> N >> M;
 
-	int ans = 0;
-	int sum = 0;
+	vector<CostEdge> edges;
+	vector<CostEdge> selected;
+	int ret = 0;
 
-	int start = 0;
-	int end = 0;
-	while (true)
+	for (int i = 0; i < M; i++)
 	{
-		if ((end >= primeNumberList.size() || start >= primeNumberList.size()) && sum < N)
-			break;
-
-		if (sum == N)
-			ans++;
-
-		if (sum >= N)
-		{
-			sum -= primeNumberList[start];
-			start++;
-		}
-		else
-		{
-			sum += primeNumberList[end];
-			end++;
-		}
+		int A, B, C;
+		cin >> A >> B >> C;
+		edges.push_back({ C, A - 1, B - 1 });
+		edges.push_back({ C, B - 1, A - 1 });
 	}
-	
-	cout << ans;
+
+	std::sort(edges.begin(), edges.end());
+
+	DisjointSet ds(N);
+
+	int lastCost = 0;
+	for(CostEdge& edge : edges)
+	{
+		if (ds.Find(edge.u) == ds.Find(edge.v))
+			continue;
+
+		ds.Merge(edge.u, edge.v);
+		selected.push_back(edge);
+		ret += edge.cost;
+		lastCost = edge.cost;
+	}
+
+	cout << ret - lastCost << endl;
 
 	return 0;
 }
