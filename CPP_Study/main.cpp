fbe83fe66d30dd3679ba40c11830b981812bf080
Sanghyun <hshf123@fowgames.com>
2024-07-16 19:58:00 +0900

    1799 - 비숍

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,75 +24,197 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-struct Problem
+int N;
+vector<vector<int>> m;
+vector<vector<int>> bishopPosList;
+int dy[] = { 1, -1, -1, 1 };
+int dx[] = { 1, 1, -1, -1 };
+int ans = 0;
+
+bool CanGo(const int& y, const int& x)
 {
-	int level = 0;
-	unordered_set<int> priorityList;
-	unordered_set<int> relationList;
-	bool solved = false;
+	if (y < 0 || x < 0 || y >= N || x >= N)
+		return false;
 
-	void Relation(const int& n)
+	return true;
+}
+void Put(const int& y, const int& x)
+{
+	if (CanGo(y, x) == false)
+		return;
+
+	bishopPosList[y][x]++;
+
+	int nextY = y;
+	int nextX = x;
+	// 오른쪽 위
+	nextY = y + dy[0];
+	nextX = x + dx[0];
+	while (CanGo(nextY, nextX))
 	{
-		relationList.insert(n);
+		bishopPosList[nextY][nextX]++;
+		nextY += dy[0];
+		nextX += dx[0];
+	}
+	// 오른쪽 아래
+	nextY = y + dy[1];
+	nextX = x + dx[1];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]++;
+		nextY += dy[1];
+		nextX += dx[1];
+	}
+	// 왼쪽 아래
+	nextY = y + dy[2];
+	nextX = x + dx[2];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]++;
+		nextY += dy[2];
+		nextX += dx[2];
+	}
+	// 왼쪽 위
+	nextY = y + dy[3];
+	nextX = x + dx[3];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]++;
+		nextY += dy[3];
+		nextX += dx[3];
+	}
+}
+void CleanUp(const int& y, const int& x)
+{
+	if (CanGo(y, x) == false)
+		return;
+
+	bishopPosList[y][x] = max(bishopPosList[y][x] - 1, 0);
+
+	int nextY = y;
+	int nextX = x;
+	// 오른쪽 위
+	nextY = y + dy[0];
+	nextX = x + dx[0];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]--;
+		nextY += dy[0];
+		nextX += dx[0];
+	}
+	// 오른쪽 아래
+	nextY = y + dy[1];
+	nextX = x + dx[1];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]--;
+		nextY += dy[1];
+		nextX += dx[1];
+	}
+	// 왼쪽 아래
+	nextY = y + dy[2];
+	nextX = x + dx[2];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]--;
+		nextY += dy[2];
+		nextX += dx[2];
+	}
+	// 왼쪽 위
+	nextY = y + dy[3];
+	nextX = x + dx[3];
+	while (CanGo(nextY, nextX))
+	{
+		bishopPosList[nextY][nextX]--;
+		nextY += dy[3];
+		nextX += dx[3];
+	}
+}
+
+void BacktrackingWhite(const int& y, const int& x, int ret)
+{
+	if (CanGo(y, x) == false)
+	{
+		ans = max(ans, ret);
+		return;
 	}
 
-	void Insert(const int& n)
+	int nextY = y;
+	int nextX = x + 2;
+	if (nextX >= N)
 	{
-		priorityList.insert(n);
+		nextY += 1;
+		if (nextY % 2 == 0)
+			nextX = 0;
+		else
+			nextX = 1;
 	}
 
-	void Erase(const int& n)
+	if (m[y][x] == 1 && bishopPosList[y][x] == 0)
 	{
-		priorityList.erase(n);
+		// 놓는다
+		Put(y, x);
+		BacktrackingWhite(nextY, nextX, ret + 1);
+		CleanUp(y, x);
 	}
-};
+
+	// 안놓는다
+	BacktrackingWhite(nextY, nextX, ret);
+}
+
+void BacktrackingBlack(const int& y, const int& x, int ret)
+{
+	if (CanGo(y, x) == false)
+	{
+		ans = max(ans, ret);
+		return;
+	}
+
+	int nextY = y;
+	int nextX = x + 2;
+	if (nextX >= N)
+	{
+		nextY += 1;
+		if (nextY % 2 == 0)
+			nextX = 1;
+		else
+			nextX = 0;
+	}
+
+	if (m[y][x] == 1 && bishopPosList[y][x] == 0)
+	{
+		// 놓는다
+		Put(y, x);
+		BacktrackingBlack(nextY, nextX, ret + 1);
+		CleanUp(y, x);
+	}
+
+	// 안놓는다
+	BacktrackingBlack(nextY, nextX, ret);
+}
 
 int main()
 {
 	Init;
 
-	int N, M;
-	cin >> N >> M;
-	vector<Problem> problemList(N + 1);
-	for (int i = 0; i < N; i++)
-		problemList[i + 1].level = i + 1;
-	for (int i = 0; i < M; i++)
+	cin >> N;
+	m = vector<vector<int>>(N, vector<int>(N));
+	bishopPosList = vector<vector<int>>(N, vector<int>(N, 0));
+	for (int y = 0; y < N; y++)
 	{
-		int A, B;
-		cin >> A >> B;
-		problemList[B].Insert(A);
-		problemList[A].Relation(B);
-	}
-
-	priority_queue<int, vector<int>, greater<int>> pq;
-	for (int i = 1; i <= N; i++)
-	{
-		if (problemList[i].priorityList.empty())
-			pq.push(i);
-	}
-
-	int solveCount = 0;
-	while (pq.empty() == false)
-	{
-		int level = pq.top();
-		pq.pop();
-
-		if (problemList[level].solved)
-			continue;
-
-		cout << level << " ";
-		problemList[level].solved = true;
-		if (++solveCount == N)
-			break;
-
-		// 얘가 풀리면 영향있는 문제들
-		for (const int& lv : problemList[level].relationList)
+		for (int x = 0; x < N; x++)
 		{
-			problemList[lv].Erase(level);
-			if (problemList[lv].priorityList.empty())
-				pq.push(lv);
+			cin >> m[y][x];
 		}
 	}
 
+	int sum = 0;
+	BacktrackingWhite(0, 0, 0);
+	sum += ans;
+	ans = 0;
+	BacktrackingBlack(0, 1, 0);
+	sum += ans;
+
+	cout << sum;
 	return 0;
 }
