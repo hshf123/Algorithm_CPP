e3681b8167bdc6d06063f09e1f163108c0c86f10
Sanghyun <hshf123@fowgames.com>
2024-07-17 21:05:53 +0900

    알고리즘문제해결전략 - BOGGLE

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,45 +24,75 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
+// 시계방향 8방
+array<int, 8> dy = { 1, 1, 0, -1, -1, -1, 0, 1 };
+array<int, 8> dx = { 0, 1, 1, 1, 0, -1, -1, -1 };
+vector<vector<char>> board(5, vector<char>(5));
+vector<string> tc;
+
+bool Search(const int& y, const int& x, const string& str)
+{
+	// 범위를 벗어났다면 실패
+	if (y < 0 || x < 0 || y >= 5 || x >= 5)
+		return false;
+
+	const char& c = board[y][x];
+	if (str[0] != c)		// 틀린문자
+		return false;
+	if (str.size() == 1)	// 다 찾음!
+		return true;
+
+	// 다음 8방향 탐색
+	for (int i = 0; i < 8; i++)
+	{
+		// 먼저 한 번이라도 찾으면 끝
+		if (Search(y + dy[i], x + dx[i], str.substr(1)))
+			return true;
+	}
+
+	return false;
+}
+
 int main()
 {
 	Init;
 
-	int N, S;
-	cin >> N >> S;
+	int C;
+	cin >> C;
 
-	vector<int> seq(N);
+	for (int y = 0; y < 5; y++)
+		for (int x = 0; x < 5; x++)
+			cin >> board[y][x];
 
-	for (int i = 0; i < N; i++)
-		cin >> seq[i];
-
-	int ans = INT32_MAX;
-	
-	int s = 0;
-	int e = 1;
-	int sum = seq[s];
-	while (true)
+	for (int c = 0; c < C; c++)
 	{
-		if (e > N || s > e)
-			break;
+		int N;
+		cin >> N;
+		tc.resize(N);
+		for (int i = 0; i < N; i++)
+			cin >> tc[i];
 
-		if (sum >= S)
+		vector<bool> existList(tc.size(), false);
+		for (int y = 0; y < 5; y++)
 		{
-			ans = min(ans, e - s);
-			sum -= seq[s];
-			s++;
+			for (int x = 0; x < 5; x++)
+			{
+				for (int i = 0; i < tc.size(); i++)
+				{
+					if (Search(y, x, tc[i]))
+						existList[i] = true;
+				}
+			}
 		}
-		else
-		{
-			if (e == N)
-				break;
 
-			sum += seq[e];
-			e++;
+		for (int i = 0; i < tc.size(); i++)
+		{
+			if (existList[i])
+				cout << tc[i] << " YES" << endl;
+			else
+				cout << tc[i] << " NO" << endl;
 		}
 	}
-
-	cout << (ans == INT32_MAX ? 0 : ans);
 	
 	return 0;
 }
