227d1fe2f0127ee308cbf6bea9504ec63fb26dfc
Sanghyun <hshf123@fowgames.com>
2024-10-21 18:14:18 +0900

    숨겨진 합

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,60 +24,41 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-struct Study
-{
-	int st = 0;
-	int et = 0;
-	int e = 0;
 
-	bool operator<(const Study& other)
-	{
-		return et < other.et;
-	}
-};
-
-int N, M, R;
-vector<int> cache;  // N시간 까지 최대 효율
-vector<Study>  vec;
-
-int ans = 0;
-
-void DP()
-{
-	for (int i = 0; i < M; i++)
-	{
-		Study& study = vec[i];
-		int& ret = cache[i];
-		ret = study.e;
-		for (int j = i - 1; j >= 0; j--)
-		{
-			Study& before = vec[j];
-			if (before.et + R <= study.st)
-				ret = max(ret, cache[j] + study.e);
-		}
-
-		ans = max(ans, ret);
-	}
-}
 
 int main()
 {
 	Init;
 	
-	cin >> N >> M >> R;
-	cache.resize(M, -1);
-	vec.resize(M);
-	for (int i = 0; i < M; i++)
+	string str;
+	cin >> str;
+
+	int64 sum = 0;
+
+	string num;
+
+	for (int i = 0; i < str.size(); i++)
 	{
-		int st, et, e;
-		cin >> st >> et >> e;
-		vec[i] = { st, et, e };
+		char& c = str[i];
+		if (isdigit(c))
+		{
+			num += c;
+		}
+		else
+		{
+			if (num.empty())
+				continue;
+
+			sum += atoi(num.c_str());
+			num.clear();
+		}
 	}
 
-	sort(vec.begin(), vec.end());
-
-	DP();
+	if (num.empty() == false)
+	{
+		sum += atoi(num.c_str());
+	}
+	cout << sum;
 	
-	cout << ans;
 	return 0;
 }
