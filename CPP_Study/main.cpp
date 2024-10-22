18a45a659728313e4054c2acf90cf4b1bb81f32c
Sanghyun <hshf123@fowgames.com>
2024-10-22 17:36:25 +0900

    최대선호음식 (DFS)

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,111 +24,67 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-struct Pos
+struct Student
 {
-	int y = 0;
-	int x = 0;
 	int cnt = 0;
 	int flag = 0;
 };
 
-int W, H;
-vector<vector<int>> vec;
-vector<vector<vector<int>>> visited;
-Pos startPos;
-Pos knightPos;
-vector<Pos> strawberryPosList;
+int N, D, K;
+vector<Student> vec;
 
-int dy[] = { 1,0,-1,0 };
-int dx[] = { 0,1,0,-1 };
-
-bool CanMove(const int& y, const int& x, const int& flag)
+int GetCount(int flag)
 {
-	if (y >= H || x >= W || x < 0 || y < 0)
-		return false;
+	int ret = 0;
+	for (Student& s : vec)
+	{
+		if (s.flag == (flag & s.flag))
+			ret++;
+	}
 
-	if (vec[y][x] == 1)
-		return false;
-
-	if (visited[flag][y][x] != 0)
-		return false;
-
-	if (flag == 0 && vec[y][x] == 3)
-		return false;
-
-	return true;
+	return ret;
 }
 
-void BFS()
+int DFS(const int& cnt, const int& idx, const int& flag)
 {
-	queue<Pos> q;
-	q.push(startPos);
-	q.push(knightPos);
-	while (q.empty() == false)
+	if (cnt == K)
 	{
-		Pos p = q.front();
-		q.pop();
-		int y = p.y;
-		int x = p.x;
-		int cnt = p.cnt;
-		int flag = p.flag;
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nextY = y + dy[i];
-			int nextX = x + dx[i];
-			if (CanMove(nextY, nextX, flag) == false)
-				continue;
-
-			// 꺼낼 때 말고 여기서 방문체크 해줘야 훨씬 빠름 (중복 방지)
-			visited[flag][nextY][nextX] = cnt + 1;
-			q.push({ nextY, nextX, cnt + 1, flag });
-		}
+		return GetCount(flag);
 	}
+
+	int ret = 0;
+	for (int i = idx; i <= D; i++)
+	{
+		if ((flag & (1 << i)))
+			continue;
+		ret = max(ret, DFS(cnt + 1, i + 1, flag | (1 << i)));
+	}
+	return ret;
 }
 
 int main()
 {
 	Init;
 
-	cin >> W >> H;
-	vec = vector<vector<int>>(H, vector<int>(W));
-	visited = vector<vector<vector<int>>>(2, vector<vector<int>>(H, vector<int>(W)));
-	for (int y = 0; y < H; y++)
+	cin >> N >> D >> K;
+	vec.resize(N);
+	for (int i = 0; i < N; i++)
 	{
-		for (int x = 0; x < W; x++)
+		int cnt;
+		cin >> cnt;
+		Student s;
+		s.cnt = cnt;
+		for (int j = 0; j < cnt; j++)
 		{
-			int n;
-			cin >> n;
-			vec[y][x] = n;
-			switch (n)
-			{
-			case 2:
-				startPos = { y,x,1,0 };
-				break;
-			case 3:
-				knightPos = { y,x,1,1 };
-				break;
-			case 4:
-				strawberryPosList.push_back({ y,x });
-				break;
-			default:
-				break;
-			}
+			int flag;
+			cin >> flag;
+			s.flag |= (1 << flag);
 		}
+
+		vec[i] = s;
 	}
 
-	BFS();
-
-	int ret = INT32_MAX;
-	for (Pos& pos : strawberryPosList)
-	{
-		if (visited[0][pos.y][pos.x] == 0 || visited[1][pos.y][pos.x] == 0)
-			continue;
-		ret = min(ret, visited[0][pos.y][pos.x] - 1 + visited[1][pos.y][pos.x] - 1);
-	}
-
+	int ret = DFS(0, 1, 0);
 	cout << ret;
-
 	return 0;
 }
