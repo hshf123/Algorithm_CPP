b1a58d4434fd36e548c22e16d266fe568c8ae36f
Sanghyun <hshf123@fowgames.com>
2024-10-23 10:53:21 +0900

    톰과 제리

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,82 +24,83 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-vector<vector<int>> sdoku(9, vector<int>(9, 0));
-vector<pair<int, int>> vec;
-
-unordered_set<int> GetEnableNumberList(const int& y, const int& x)
+struct Pos
 {
-	unordered_set<int> ret = { 1,2,3,4,5,6,7,8,9 };
-	for (int i = 0; i < 9; i++)
-		ret.erase(sdoku[y][i]);
-	for (int i = 0; i < 9; i++)
-		ret.erase(sdoku[i][x]);
-	for (int i = 0; i < 3; i++)
+	int y = 0;
+	int x = 0;
+	int dir = -1;
+	int cnt = 0;
+
+	bool operator==(const Pos& other) const
 	{
-		for (int j = 0; j < 3; j++)
+		return y == other.y && x == other.x;
+	}
+};
+
+int n;
+vector<vector<int>> vec;
+vector<vector<int>> visited;
+Pos tom;
+Pos jerry;
+int ans = INT32_MAX;
+
+int dy[] = { 1,0,-1,0 };
+int dx[] = { 0,1,0,-1 };
+
+void BFS()
+{
+	queue<Pos> q;
+	q.push(tom);
+	while (q.empty() == false)
+	{
+		auto p = q.front();
+		q.pop();
+
+		if (p == jerry)
+			ans = min(ans, p.cnt);
+
+		for (int i = 0; i < 4; i++)
 		{
-			ret.erase(sdoku[y - (y % 3) + j][x - (x % 3) + i]);
+			int ny = p.y + dy[i];
+			int nx = p.x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= n || vec[ny][nx] == 1)
+				continue;
+			int ncnt = p.cnt;
+			if (i != p.dir)
+				ncnt++;
+			if (visited[ny][nx] < ncnt)
+				continue;
+			visited[ny][nx] = ncnt;
+			q.push(Pos{ ny, nx, i, ncnt });
 		}
 	}
-
-	return ret;
-}
-bool Check()
-{
-	for (int y = 0; y < 9; y++)
-	{
-		for (int x = 0; x < 9; x++)
-		{
-			if (sdoku[y][x] == 0)
-				return false;
-		}
-	}
-
-	return true;
-}
-
-bool DFS(const int& idx)
-{
-	if (idx == vec.size())
-		return true;
-
-	unordered_set<int> s = GetEnableNumberList(vec[idx].first, vec[idx].second);
-	for (const int& p : s)
-	{
-		int& n = sdoku[vec[idx].first][vec[idx].second];
-		n = p;
-		if (DFS(idx + 1))
-			return true;
-		n = 0;
-	}
-
-	return false;
 }
 
 int main()
 {
 	Init;
 
-	for (int y = 0; y < 9; y++)
+	cin >> n;
+	vec = vector<vector<int>>(n, vector<int>(n, 0));
+	visited = vector<vector<int>>(n, vector<int>(n, INT32_MAX));
+	for (int y = 0; y < n; y++)
 	{
-		for (int x = 0; x < 9; x++)
+		for (int x = 0; x < n; x++)
 		{
-			cin >> sdoku[y][x];
-			if (sdoku[y][x] == 0)
-				vec.push_back({ y,x });
+			cin >> vec[y][x];
 		}
 	}
 
-	DFS(0);
-
-	for (int y = 0; y < 9; y++)
-	{
-		for (int x = 0; x < 9; x++)
-		{
-			cout << sdoku[y][x] << " ";
-		}
-		cout << endl;
-	}
+	int ty, tx, jy, jx;
+	cin >> ty >> tx >> jy >> jx;
+	tom.y = ty - 1;
+	tom.x = tx - 1;
+	jerry.y = jy - 1;
+	jerry.x = jx - 1;
 	
+	BFS();
+
+	cout << (ans == INT32_MAX ? -1 : ans - 1);
+
 	return 0;
 }
