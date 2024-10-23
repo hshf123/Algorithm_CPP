b6e91b1629e2b80200336aac01a19a4fc8c1ee12
Sanghyun <hshf123@fowgames.com>
2024-10-23 15:41:27 +0900

    모든쌍 괄호 구하기

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,73 +24,48 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-struct Pos
+int N;
+int ret = 0;
+
+void DFS(int cnt, int idx, int flag)
 {
-	int y = 0;
-	int x = 0;
-};
-
-int N, M, K;
-vector<vector<int>> vec;
-vector<vector<bool>> visited;
-
-int dy[] = { 1,0,-1,0 };
-int dx[] = { 0,1,0,-1 };
-
-int BFS(const int& y, const int& x)
-{
-	if (visited[y][x])
-		return 0;
-
-	queue<Pos> q;
-	q.push({ y,x });
-	visited[y][x] = true;
-	int ret = 0;
-	while (q.empty() == false)
+	if (cnt == N)
 	{
-		int y = q.front().y;
-		int x = q.front().x;
-		q.pop();
-		ret++;
-
-		for (int i = 0; i < 4; i++)
+		int n = 0;
+		string ans;
+		for (int i = 1; i < N * 2 + 1; i++)
 		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny < 0 || nx < 0 || ny >= N || nx >= M)
-				continue;
-			if (vec[ny][nx] == 0)
-				continue;
-			if (visited[ny][nx])
-				continue;
-			visited[ny][nx] = true;
-			q.push({ ny,nx });
+			if (flag & (1 << i))
+			{
+				ans += "(";
+				n++;
+			}
+			else
+			{
+				ans += ")";
+				n--;
+			}
+			if (n < 0)
+				return;
 		}
+		ret++;
+		cout << ans << endl;
+		return;
 	}
-	return ret;
+
+	for (int i = idx + 1; i < N * 2; i++)
+	{
+		DFS(cnt + 1, i, (flag | (1 << i)));
+	}
 }
 
 int main()
 {
 	Init;
 
-	cin >> N >> M >> K;
-	vec = vector<vector<int>>(N, vector<int>(M, 0));
-	visited = vector<vector<bool>>(N, vector<bool>(M, false));
-	vector<pair<int, int>> posList;
-	for (int i = 0; i < K; i++)
-	{
-		int y, x;
-		cin >> y >> x;
-		vec[y - 1][x - 1] = 1;
-		posList.push_back({ y - 1, x - 1 });
-	}
+	cin >> N;
+	DFS(1, 1, 2);
+	cout << ret;
 
-	int ans = 0;
-	for (const auto& p : posList)
-	{
-		ans = max(ans, BFS(p.first, p.second));
-	}
-	cout << ans;
 	return 0;
 }
