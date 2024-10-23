5f92377284227e5a81bf20bff7f4a212dcd1b9ca
Sanghyun <hshf123@fowgames.com>
2024-10-23 18:56:33 +0900

    문자열점수 - DP 응용

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,24 +24,36 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
+int n1, n2, n3;
 
 int main()
 {
 	Init;
-
+	cin >> n1 >> n2 >> n3;
 	string A;
 	string B;
 	cin >> A >> B;
 	vector<vector<int>> vec(A.size() + 1, vector<int>(B.size() + 1, 0));
 
+	for (int i = 1; i < A.size() + 1; i++)
+		vec[i][0] = i * n2;
+	for (int i = 1; i < B.size() + 1; i++)
+		vec[0][i] = i * n2;
+
 	for (int y = 1; y < A.size() + 1; y++)
 	{
 		for (int x = 1; x < B.size() + 1; x++)
 		{
+			int& val = vec[y][x];
 			if (A[y - 1] == B[x - 1])
-				vec[y][x] = vec[y - 1][x - 1] + 1;
+				val = vec[y - 1][x - 1] + n1;
 			else
-				vec[y][x] = max(vec[y - 1][x], vec[y][x - 1]);
+			{
+				val = vec[y - 1][x] + n2;
+				val = max(val, vec[y][x - 1] + n2);
+				val = max(val, vec[y - 1][x - 1] + (n2 * 2));
+				val = max(val, vec[y - 1][x - 1] + n3);
+			}
 		}
 	}
 
