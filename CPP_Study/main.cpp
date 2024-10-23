c4f56d4c9ed7298dcd23f53ad9a152d67b050fcb
Sanghyun <hshf123@fowgames.com>
2024-10-23 20:47:27 +0900

    팀만들기

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,40 +24,47 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-int n1, n2, n3;
+vector<int> vec(9, 0);
+unordered_map<int, unordered_set<int>> team;
+
+int DFS(int idx)
+{
+	if (idx == 9)
+	{
+		vector<int> sum(3, 0);
+		for (auto& p : team)
+		{
+			for (const int& p2 : p.second)
+			{
+				sum[p.first] += vec[p2];
+			}
+		}
+
+		sort(sum.begin(), sum.end());
+		return sum.back() - sum.front();
+	}
+
+	int ret = INT32_MAX;
+	for (int i = 0; i < 3; i++)
+	{
+		if(team[i].size() < 3)
+		{
+			team[i].insert(idx);
+			ret = min(ret, DFS(idx + 1));
+			team[i].erase(idx);
+		}
+	}
+	return ret;
+}
 
 int main()
 {
 	Init;
-	cin >> n1 >> n2 >> n3;
-	string A;
-	string B;
-	cin >> A >> B;
-	vector<vector<int>> vec(A.size() + 1, vector<int>(B.size() + 1, 0));
+	
+	for (int i = 0; i < 9; i++)
+		cin >> vec[i];
 
-	for (int i = 1; i < A.size() + 1; i++)
-		vec[i][0] = i * n2;
-	for (int i = 1; i < B.size() + 1; i++)
-		vec[0][i] = i * n2;
-
-	for (int y = 1; y < A.size() + 1; y++)
-	{
-		for (int x = 1; x < B.size() + 1; x++)
-		{
-			int& val = vec[y][x];
-			if (A[y - 1] == B[x - 1])
-				val = vec[y - 1][x - 1] + n1;
-			else
-			{
-				val = vec[y - 1][x] + n2;
-				val = max(val, vec[y][x - 1] + n2);
-				val = max(val, vec[y - 1][x - 1] + (n2 * 2));
-				val = max(val, vec[y - 1][x - 1] + n3);
-			}
-		}
-	}
-
-	cout << vec[A.size()][B.size()];
+	cout << DFS(0);
 
 	return 0;
 }
