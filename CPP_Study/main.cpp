4754bfb8940ceaee7f3acf054f302c892638905c
Sanghyun <hshf123@fowgames.com>
2024-10-23 11:28:23 +0900

    거리 두기

--- CPP_Study/main.cpp
+++ CPP_Study/main.cpp
@@ -24,83 +24,41 @@ using int64 = long long;
 using uint64 = unsigned long long;
 #pragma endregion
 
-struct Pos
-{
-	int y = 0;
-	int x = 0;
-	int dir = -1;
-	int cnt = 0;
-
-	bool operator==(const Pos& other) const
-	{
-		return y == other.y && x == other.x;
-	}
-};
-
-int n;
-vector<vector<int>> vec;
-vector<vector<int>> visited;
-Pos tom;
-Pos jerry;
-int ans = INT32_MAX;
-
-int dy[] = { 1,0,-1,0 };
-int dx[] = { 0,1,0,-1 };
-
-void BFS()
-{
-	queue<Pos> q;
-	q.push(tom);
-	while (q.empty() == false)
-	{
-		auto p = q.front();
-		q.pop();
-
-		if (p == jerry)
-			ans = min(ans, p.cnt);
-
-		for (int i = 0; i < 4; i++)
-		{
-			int ny = p.y + dy[i];
-			int nx = p.x + dx[i];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= n || vec[ny][nx] == 1)
-				continue;
-			int ncnt = p.cnt;
-			if (i != p.dir)
-				ncnt++;
-			if (visited[ny][nx] < ncnt)
-				continue;
-			visited[ny][nx] = ncnt;
-			q.push(Pos{ ny, nx, i, ncnt });
-		}
-	}
-}
+int N;
 
 int main()
 {
 	Init;
 
-	cin >> n;
-	vec = vector<vector<int>>(n, vector<int>(n, 0));
-	visited = vector<vector<int>>(n, vector<int>(n, INT32_MAX));
-	for (int y = 0; y < n; y++)
+	int maxCnt = 0;
+	int cnt = 0;
+	bool c = false;
+	int sc = 0;
+	int ec = 0;
+
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		for (int x = 0; x < n; x++)
+		int n;
+		cin >> n;
+		if (n == 1)
 		{
-			cin >> vec[y][x];
+			c = true;
+			cnt = 0;
+			continue;
 		}
+		if (c == false)
+			sc++;
+		cnt++;
+		maxCnt = max(cnt, maxCnt);
+		if (i == N - 1 && n == 0)
+			ec = cnt;
 	}
 
-	int ty, tx, jy, jx;
-	cin >> ty >> tx >> jy >> jx;
-	tom.y = ty - 1;
-	tom.x = tx - 1;
-	jerry.y = jy - 1;
-	jerry.x = jx - 1;
-	
-	BFS();
+	maxCnt = max(max(maxCnt / 2 + (maxCnt % 2), sc), ec);
 
-	cout << (ans == INT32_MAX ? -1 : ans - 1);
+	cout << maxCnt;
 
 	return 0;
 }
